a10c7d831dd1335173a1d5c73c73d62931fb24dc
tomas-quix <78492422+tomas-quix@users.noreply.github.com>
2022-01-04 15:53:55 +0100

    Update main.py

--- python/model/Data-Filtering/main.py
+++ python/model/Data-Filtering/main.py
@@ -21,26 +21,13 @@ def read_stream(new_stream: StreamReader):
     
     stream_writer.properties.parents.append(new_stream.stream_id)
 
-    buffer = new_stream.parameters.create_buffer("Brake")
-    buffer.time_span_in_milliseconds = 100  # React to 100ms windows of data.
+    quix_function = QuixFunction(stream_writer, new_stream)
 
-    # Callback triggered for each new data frame
-    def on_parameter_data_handler(data: ParameterData):
-
-        df = data.to_panda_frame()  # Input data frame
-        output_df = pd.DataFrame()
-        output_df["time"] = df["time"]
-
-        output_df["TAG__LapNumber"] = df["TAG__LapNumber"]
-        print(df)
-
-        # If braking force applied is more than 50%, we send True.  
-        output_df["HardBraking"] = df.apply(lambda row: "True" if row.Brake > 0.5 else "False", axis=1)  
-
-        stream_writer.parameters.buffer.write(output_df)  # Send filtered data to output topic
-
+        
     # React to new data received from input topic.
-    buffer.on_read += on_parameter_data_handler
+    new_stream.events.on_read += quix_function.on_event_data_handler
+
+    new_stream.parameters.on_read += quix_function.on_parameter_data_handler
 
     # When input stream closes, we close output stream as well. 
     def on_stream_close(endType: StreamEndType):
@@ -48,12 +35,6 @@ def read_stream(new_stream: StreamReader):
         print("Stream closed:" + stream_writer.stream_id)
 
     new_stream.on_stream_closed += on_stream_close
-
-    # React to any metadata changes.
-    def stream_properties_changed():
-        stream_writer.properties.name = new_stream.properties.name + " hard braking"
-
-    new_stream.properties.on_changed += stream_properties_changed
 
 
 # Hook up events before initiating read to avoid losing out on any data
@@ -84,3 +65,4 @@ while not event.is_set():
     time.sleep(1)
 
 print('Exiting')
+
