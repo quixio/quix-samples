22f90643c68851e5cc0c118a573bce666c3109e0
Steve <steve@quix.ai>
2022-01-06 10:32:48 +0000

    Realtime car data sample

--- python/write/Car-Data-Realtime/main.py
+++ python/write/Car-Data-Realtime/main.py
@@ -1,4 +1,5 @@
 from quixstreaming import QuixStreamingClient
+import time
 import pandas as pd
 import os
 
@@ -10,7 +11,7 @@ client = QuixStreamingClient('{placeholder:token}')
 print("Opening output topic")
 output_topic = client.open_output_topic(os.environ["output"])
 
-# Create a new stream. A stream is a collection of data that belong to a single session of a single source.
+# Create a new output_stream. A stream is a collection of data that belong to a single session of a single source.
 # For example single car journey.
 # If you don't specify a stream id, a random guid is used.
 output_stream = output_topic.create_stream()
@@ -19,10 +20,10 @@ output_stream = output_topic.create_stream()
 # stream = output_topic.create_stream("my-own-stream-id")
 
 # Give the stream human readable name. This name will appear in data catalogue.
-output_stream.properties.name = "cardata"
+output_stream.properties.name = "cardata realtime"
 
 # Save stream in specific folder in data catalogue to help organize your workspace.
-output_stream.properties.location = "/static data"
+output_stream.properties.location = "/simulations"
 
 # Add stream metadata to add context to time series data.
 output_stream.properties.metadata["circuit"] = "Sakhir Short"
@@ -33,11 +34,22 @@ output_stream.properties.metadata["game"] = "Codemasters F1 2019"
 df = pd.read_csv("cardata.csv")
 
 # Add TAG__ prefix to column LapNumber to use this column as tag (index).
-df = df.rename(columns={"LapNumber": "TAG__LapNumber" })
+df = df.rename(columns={"LapNumber" : "TAG__LapNumber" })
 
-# Write data frame to output topic.
+# Add optional metadata to parameters.
+output_stream.parameters.add_definition("Speed").set_range(0, 400).set_unit("KMH")
+output_stream.parameters.add_definition("Gear").set_range(0, 9)
+output_stream.parameters.add_definition("Steer").set_range(-1, 1)
+output_stream.parameters.add_definition("EngineRPM").set_range(0, 14000)
+
+# Every second we read one second worth of data from data frame and send it to the platform.
 print("Writing data")
-output_stream.parameters.write(df)
+for i in range(df["Timestamp"].count()):
+    start = time.time()
+    sub_df = df.head(i).tail(1)
+    output_stream.parameters.write(sub_df)
+    print("Sending " + str(i) + "/" + str(df["Timestamp"].count()))
+    time.sleep(max(0.0, 1 - (time.time() - start)))
 
 print("Closing stream")
 
