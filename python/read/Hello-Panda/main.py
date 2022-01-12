e90a51533dc062914119152dc2e5a7f88b29c3a4
Steve <steve@quix.ai>
2022-01-12 11:40:37 +0000

    Added Helo Pandas read sample

--- python/read/Hello-Panda/main.py
+++ python/read/Hello-Panda/main.py
@@ -1,7 +1,6 @@
 from quixstreaming import QuixStreamingClient, StreamReader
 from quixstreaming.models.parametersbufferconfiguration import ParametersBufferConfiguration
 from quixstreaming.app import App
-from quix_function import QuixFunction
 import os
 
 # Create a client to help you to create input reader or output writer for specified topic.
@@ -12,19 +11,22 @@ input_topic = client.open_input_topic(os.environ["input"])
 
 
 # read streams
-def read_stream(input_stream: StreamReader):
-    print("New stream read:" + input_stream.stream_id)
+def read_stream(new_stream: StreamReader):
+    print("New stream read:" + new_stream.stream_id)
 
     buffer_options = ParametersBufferConfiguration()
     buffer_options.time_span_in_milliseconds = 100
 
-    buffer = input_stream.parameters.create_buffer(buffer_options)
+    buffer = new_stream.parameters.create_buffer(buffer_options)
 
     # handle the data in a function to simplify the example
     quix_function = QuixFunction()
 
     buffer.on_read += quix_function.on_parameter_data_handler
 
+
+# Hook up events before initiating read to avoid losing out on any data
+input_topic.on_stream_received += read_stream
 
 # Hook up events before initiating read to avoid losing out on any data
 input_topic.on_stream_received += read_stream
