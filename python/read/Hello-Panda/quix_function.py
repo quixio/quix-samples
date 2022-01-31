83f8f029ac999ddd279dd0679b8b6567d0b8f293
Steve <steve@quix.ai>
2022-01-31 15:50:20 +0000

    update hello pandas read sample

--- python/read/Hello-Panda/quix_function.py
+++ python/read/Hello-Panda/quix_function.py
@@ -4,7 +4,7 @@ from quixstreaming import ParameterData
 class QuixFunction:
 
     # Callback triggered for each new parameter data.
-    def on_parameter_data_handler(data: ParameterData):
+    def on_parameter_data_handler(self, data: ParameterData):
 
         df = data.to_panda_frame()
         print(df.to_string())
