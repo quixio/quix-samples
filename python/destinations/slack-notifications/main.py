cdb936e8004afd795f100638446ad5b0df16c923
Steve R <steve@quix.ai>
2022-02-16 12:05:35 +0000

    make slack alerting more generic

--- python/destinations/slack-notifications/main.py
+++ python/destinations/slack-notifications/main.py
@@ -4,13 +4,15 @@ from quix_function import QuixFunction
 import os
 
 
-# Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
+# Quix injects credentials automatically to the client.
+# Alternatively, you can always pass an SDK token manually as an argument.
 client = QuixStreamingClient()
 
 print("Opening input and output topics")
 input_topic = client.open_input_topic(os.environ["input"], "default-consumer-group")
 
 webhook_url = os.environ["webhook_url"]
+
 
 # Callback called for each incoming stream
 def read_stream(input_stream: StreamReader):
@@ -21,6 +23,7 @@ def read_stream(input_stream: StreamReader):
         
     # React to new data received from input topic.
     input_stream.parameters.on_read += quix_function.on_parameter_data_handler
+
 
 # Hook up events before initiating read to avoid losing out on any data
 input_topic.on_stream_received += read_stream
--- python/destinations/slack-notifications/quix_function.py
+++ python/destinations/slack-notifications/quix_function.py
@@ -1,22 +1,22 @@
 from quixstreaming import StreamReader, ParameterData
 import requests
 
+
 class QuixFunction:
     def __init__(self, webhook_url, input_stream: StreamReader):
         self.webhook_url = webhook_url
         self.input_stream = input_stream
-        self.last_value = False
 
     # Callback triggered for each new parameter data.
     def on_parameter_data_handler(self, data: ParameterData):
 
         # change the parameter name to whatever you want
-        current_value = data.timestamps[0].parameters['HardBraking'].string_value
+        value = data.timestamps[0].parameters['SlackAlert'].string_value
 
-        # define any logic you need
-        if self.last_value != current_value:
-            self.last_value = current_value
-            
-            # send your slack message
-            slack_message = {"text": "ALERT:{}".format(current_value)}
-            requests.post(self.webhook_url, json=slack_message)
+        # define any logic you need or keep the logic elsewhere
+
+        print("Sending an alert {} {}".format(value, self.webhook_url))
+
+        # send your Slack message
+        slack_message = {"text": value}
+        requests.post(self.webhook_url, json=slack_message)
