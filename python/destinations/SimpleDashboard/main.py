be58bc9197f73b84df88888f82af2472da66f327
Tomas Neubauer <tomas@quix.ai>
2022-03-04 16:46:00 +0100

    Simple dashboard with Flask

---
+++ python/destinations/SimpleDashboard/main.py
@@ -0,0 +1,55 @@
+from quixstreaming import QuixStreamingClient, StreamReader
+from quixstreaming.app import App
+from quix_function import QuixFunction
+from flask import Flask
+import threading
+import os
+
+streams = {}
+app = Flask("Dashboard")
+
+
+@app.route("/dashboard/<streamId>")
+def get_board(streamId:str):
+    
+    board = streams[streamId]
+    return board.dashboard[["TAG__LapNumber","Speed"]].to_html(classes='data'), 200
+
+
+def data_sub():
+
+    # Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
+    client = QuixStreamingClient()
+
+    print("Opening input topic")
+    input_topic = client.open_input_topic(os.environ["input"])
+
+
+    # read streams
+    def read_stream(input_stream: StreamReader):
+        print("New stream read:" + input_stream.stream_id)
+
+        # handle the data in a function to simplify the example
+        quix_function = QuixFunction()
+        streams[input_stream.stream_id] = quix_function
+        input_stream.parameters.on_read += quix_function.on_parameter_data_handler
+
+
+    # Hook up events before initiating read to avoid losing out on any data
+    input_topic.on_stream_received += read_stream
+
+    # Hook up to termination signal (for docker image) and CTRL-C
+    print("Listening to streams. Press CTRL-C to exit.")
+    
+    # Handle graceful exit
+    App.run()
+
+
+
+t = threading.Thread(target=data_sub)
+t.daemon = True
+t.start()
+
+
+
+app.run(debug=True, host="0.0.0.0", port=80)
---
+++ python/destinations/SimpleDashboard/quix_function.py
@@ -0,0 +1,31 @@
+from quixstreaming import ParameterData
+import pandas as pd
+
+class QuixFunction:
+
+    def __init__(self):
+        self.dashboard = pd.DataFrame()
+
+    # Callback triggered for each new parameter data.
+    def on_parameter_data_handler(self, data: ParameterData):
+        
+        df = data.to_panda_frame()
+
+        if "Speed" in df.columns:
+            df = df.append(self.dashboard)
+
+            def aggregate(x: pd.Series):
+                row = {
+                    "time": x['time'].iloc[0],
+                    "Speed": x["Speed"].max(),
+                    "TAG__LapNumber": x["TAG__LapNumber"].iloc[0]
+                }
+                return pd.Series(row)
+
+            self.dashboard = df.groupby("TAG__LapNumber").apply(aggregate)
+
+            #print(self.dashboard[["TAG__LapNumber", "Speed"]])
+
+
+
+        
