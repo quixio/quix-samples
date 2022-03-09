338e8504f346496483d024d312f4aa34f840a3ee
Tomas Neubauer <tomas@quix.ai>
2022-03-09 22:02:40 +0100

    main code

--- python/sources/Netatmo/main.py
+++ python/sources/Netatmo/main.py
@@ -1,16 +1,10 @@
 from quixstreaming import QuixStreamingClient
-from quixstreaming.app import App
-from twitter_function import TwitterFunction
+from netatmo_auth_helper import NetatmoAuthHelper
+import time
+import os
 import requests
 import json
-import time
-from requests.exceptions import ChunkedEncodingError
-import traceback
-from threading import Thread
-import os
-
-# should the main loop run?
-run = True
+import urllib.parse
 
 # Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
 client = QuixStreamingClient()
@@ -18,162 +12,97 @@ client = QuixStreamingClient()
 # Open the output topic where to write data out
 output_topic = client.open_output_topic(os.environ["output"])
 
-# Twitter bearer token goes here
-bearer_token = "{}".format(os.environ["twitter_bearer_token"])
+auth_helper = NetatmoAuthHelper(
+    os.environ["client_id"], 
+    os.environ["client_secret"],
+    os.environ["username"],
+    os.environ["password"])
 
-if bearer_token is None:
-    raise ValueError('You need to set the twitter_bearer_token to your Twitter developer bearer token')
+while True:
 
-# Twitter search parameters
-# Change this to whatever you are interested in
-twitter_search = "(#BTC OR btc OR #btc OR BTC)"
+    token = auth_helper.get_token()
 
+    device_id = os.environ["device_id"]
 
-# define code to create the output stream
-def create_stream():
-    output_stream = output_topic.create_stream()
-    output_stream.properties.name = "twitter_stream_results"
-    output_stream.properties.location = "/twitter_data"
+    url = "https://api.netatmo.com/api/getstationsdata?device_id={0}&get_favorites=false".format(urllib.parse.quote_plus(device_id))
 
-    return output_stream
+    response = requests.get(url, headers={"Authorization": "Bearer " + token})
+    response_body = response.content
+    repsonse_json = json.loads(response_body.decode("utf-8"))
 
+    devices = repsonse_json["body"]["devices"]
 
-# define the code to create the headers for the http connection
-def create_headers(token):
-    if token is None:
-        raise Exception("Bearer token not set")
+    for device in devices:
+        device_id = device["_id"]
 
-    headers = {"Authorization": "Bearer {}".format(token)}
-    return headers
+        stream = output_topic.get_or_create_stream(device_id)
+        stream.properties.name = device["home_name"]
 
+        country = device["place"]["country"]
+        city = device["place"]["city"]
 
-# define the code to get the existing rules from the twitter api
-def get_rules(headers):
-    response = requests.get(
-        "https://api.twitter.com/2/tweets/search/stream/rules", headers=headers
-    )
-    if response.status_code != 200:
-        raise Exception(
-            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
-        )
-    print("Current Rules:" + json.dumps(response.json()))
-    return response.json()
+        stream.properties.location = "{0}/{1}".format(country, city) 
 
+        dashboard_data = device["dashboard_data"]
 
-# code to delete the rules..
-def delete_all_rules(headers, rules):
-    if rules is None or "data" not in rules:
-        return None
+        time_utc = dashboard_data["time_utc"]
+        temperature = dashboard_data["Temperature"]
+        co2 = dashboard_data["CO2"]
+        humidity = dashboard_data["Humidity"]
+        noise = dashboard_data["Noise"]
+        pressure = dashboard_data["Pressure"]
+        absolute_pressure = dashboard_data["AbsolutePressure"]
+        temp_trend = dashboard_data["temp_trend"]
+        pressure_trend = dashboard_data["pressure_trend"]
 
-    ids = list(map(lambda rule: rule["id"], rules["data"]))
-    payload = {"delete": {"ids": ids}}
-    response = requests.post(
-        "https://api.twitter.com/2/tweets/search/stream/rules",
-        headers=headers,
-        json=payload
-    )
-    if response.status_code != 200:
-        raise Exception(
-            "Cannot delete rules (HTTP {}): {}".format(
-                response.status_code, response.text
-            )
-        )
-    print(json.dumps(response.json()))
+        stream.parameters.buffer.add_timestamp_nanoseconds(time_utc* 1000000000) \
+            .add_tag("country", country) \
+            .add_tag("city", city ) \
+            .add_value("temperature", temperature) \
+            .add_value("co2", co2) \
+            .add_value("humidity", humidity) \
+            .add_value("noise", noise) \
+            .add_value("pressure", pressure) \
+            .add_value("absolute_pressure", absolute_pressure) \
+            .add_value("temp_trend", temp_trend) \
+            .add_value("pressure_trend", pressure_trend) \
+            .write()
 
+        print("Device {0} telemetry sent.".format(device_id))
 
-# code to create the rules..
-def set_rules(headers):
-    sample_rules = [
-        {"value": twitter_search, "tag": "my_search"}
-    ]
-    payload = {"add": sample_rules}
-    response = requests.post(
-        "https://api.twitter.com/2/tweets/search/stream/rules",
-        headers=headers,
-        json=payload,
-    )
-    if response.status_code != 201:
-        raise Exception(
-            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
-        )
-    print(json.dumps(response.json()))
+        for module in device["modules"]:
+            name = module["module_name"]
+            battery_percent = module["battery_percent"]
+            reachable = module["reachable"]
+            firmware = module["firmware"]
+            last_seen = module["last_seen"]
+            battery_vp = module["battery_vp"]
+        
+            dashboard_data = module["dashboard_data"]
 
+            time_utc = dashboard_data["time_utc"]
+            temperature = dashboard_data["Temperature"]
+            humidity = dashboard_data["Humidity"]
+            temp_trend = dashboard_data["temp_trend"]
 
-# here were going to get the stream and handle its output
-# we'll do this by streaming the results into Quix
-def get_stream(headers, output_stream):
-    global run
+            stream.parameters.buffer.add_timestamp_nanoseconds(time_utc* 1000000000) \
+                .add_tag("country", country) \
+                .add_tag("city", city ) \
+                .add_value(name + "-temperature", temperature) \
+                .add_value(name + "-co2", co2) \
+                .add_value(name + "-humidity", humidity) \
+                .add_value(name + "-noise", noise) \
+                .add_value(name + "-pressure", pressure) \
+                .add_value(name + "-absolute_pressure", absolute_pressure) \
+                .add_value(name + "-temp_trend", temp_trend) \
+                .add_value(name + "-pressure_trend", pressure_trend) \
+                .write()
 
-    TwitterFunction.__init__(output_stream)
+            print("Module {0} telemetry sent.".format(name))
+            
 
-    while run:
-        try:
-            with requests.get(
-                    "https://api.twitter.com/2/tweets/search/stream", headers=headers, stream=True,
-            ) as response:
-                print(response.status_code)
-                if response.status_code != 200:
-                    raise Exception(
-                        "Cannot get stream (HTTP {}): {}".format(
-                            response.status_code, response.text
-                        )
-                    )
-                for response_line in response.iter_lines():
-
-                    # exit the loop if kill signal received
-                    if not run:
-                        break
-
-                    if response_line:
-                        json_response = json.loads(response_line)
-
-                        # get the data
-                        data = json_response["data"]
-                        # we will store the tag in quix too so get the rules used to obtain this data
-                        matching_rules = json_response["matching_rules"]
-
-                        TwitterFunction.data_handler(matching_rules, data)
-
-        except ChunkedEncodingError:
-            # if we get a ChunkedEncodingError error sleep then try again
-            print(traceback.format_exc())
-            time.sleep(6)
-            continue
-
-        except Exception:
-            # some unexpected error occurred.. stop the loop
-            print("Stopping loop because of un-handled error")
-            print(traceback.format_exc())
-            run = False
+    
+    time.sleep(300)
 
 
-def before_shutdown():
-    global run
 
-    # Stop the main loop
-    run = False
-
-
-def main():
-    global output_topic
-
-    headers = create_headers(bearer_token)
-    rules = get_rules(headers)
-    delete_all_rules(headers, rules)
-    set_rules(headers)
-    output_stream = create_stream()
-
-    thread = Thread(target=get_stream, args=(headers, output_stream))
-    thread.start()
-
-    # wait for sigterm
-    App.run(before_shutdown=before_shutdown)
-
-    # wait for worker thread to end
-    thread.join()
-
-    print("Exiting")
-
-
-if __name__ == "__main__":
-    main()
---
+++ python/sources/Netatmo/netatmo_auth_helper.py
@@ -0,0 +1,67 @@
+import time
+import requests
+import json
+import datetime
+
+class NetatmoAuthHelper:
+
+    def __init__(self, client_id: str, client_secret: str, username: str, password: str):
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._username = username
+        self._password = password
+        self._token = None
+
+    def get_token(self) -> str:
+        if self._token is None:
+            self._get_token()
+            
+        if time.time() - self._expire_in > 0:
+            self._refresh_token()
+
+        return self._token
+            
+
+    def _refresh_token(self):
+        payload = {
+            "grant_type": "refresh_token",
+            "client_id": self._client_id,
+            "client_secret": self._client_secret,
+            "refresh_token": self._refresh_token
+        }
+
+        response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
+
+        print(str(response.content))
+
+        response_body = json.loads(response.content)
+        self._token = response_body["access_token"]
+        self._refresh_token = response_body["refresh_token"]
+        self._expire_in = time.time() + response_body["expires_in"]
+
+        print("Refresh token acquired with validation until " + str(datetime.datetime.utcfromtimestamp(self._expire_in)))
+
+
+    def _get_token(self):
+        payload = {
+            "grant_type": "password",
+           "client_id": self._client_id,
+            "client_secret": self._client_secret,
+            "username": self._username,
+            "password": self._password
+        }
+
+        response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
+
+        
+
+        response_body = json.loads(response.content)
+        self._token = response_body["access_token"]
+        self._refresh_token = response_body["refresh_token"]
+        self._expire_in = time.time() + response_body["expires_in"]
+
+        print("Token acquired with validation until " + str(datetime.datetime.utcfromtimestamp(self._expire_in)))
+
+
+
+
