2b8782db48c8b2974b14dd1a10bce5a437fa2af2
Steve <srosam@glasswallsolutions.com>
2022-03-22 12:06:40 +0000

    Add segment connector (#157)

---
+++ python/sources/Segment-Webhook/main.py
@@ -0,0 +1,56 @@
+from quixstreaming import QuixStreamingClient
+from flask import Flask, request
+from datetime import datetime
+from waitress import serve
+import os
+import json
+import hmac
+import hashlib
+
+# Quix injects credentials automatically to the client. 
+# Alternatively, you can always pass an SDK token manually as an argument.
+client = QuixStreamingClient()
+
+# Open the output topic where to write data out
+output_topic = client.open_output_topic(os.environ["output"])
+
+stream = output_topic.create_stream()
+stream.properties.name = "Segment Data"
+
+app = Flask("Segment Webhook")
+
+# this is unauthenticated, anyone could post anything to you!
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    
+    # get the shared secret from environment variables
+    secret = os.environ["shared_secret"]
+    # convert to a byte array
+    secret_bytes = bytearray(secret, "utf-8")
+
+    # get the signature from the headers
+    header_sig = request.headers['x-signature']
+
+    # compute a hash-based message authentication code (HMAC)
+    hex_digest = hmac.new(secret_bytes, request.get_data(), hashlib.sha1).hexdigest()
+
+    # compare the HMAC to the header signature provided by Segment
+    if(header_sig != hex_digest):
+        # if they don't match its no bueno
+        return "ERROR", 401
+    
+    # if they do then fly me to the moon
+    stream.events.add_timestamp(datetime.now())\
+        .add_value(request.json["type"], json.dumps(request.json))\
+        .write()
+
+    return "OK", 200
+
+
+print("CONNECTED!")
+
+# you can use app.run for dev, but its not secure, stable or particularly efficient
+# app.run(debug=True, host="0.0.0.0", port=80)
+
+# use waitress instead for production
+serve(app, host='0.0.0.0', port=80)
