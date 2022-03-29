f7ec747d1da4496f9a3b2c865a3919fe19f9bacc
Steve R <steve@quix.ai>
2022-03-29 15:57:58 +0100

    Snowflake Destination

---
+++ csharp/destinations/snowflake/Quix.Snowflake.Writer/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Hosting;
+using Quix.Snowflake.Infrastructure.TimeSeries.Repositories;
+
+namespace Quix.Snowflake.Writer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //SnowflakeWriteRepository.foo();
+            
+            var built = CreateHostBuilder(args).Build();
+            Startup.AfterBuild(built.Services);
+            built.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) => Startup.ConfigureAppConfiguration(context, builder, args))
+                .ConfigureServices(Startup.ConfigureServices)
+                .ConfigureLogging(Startup.ConfigureLogging);
+    }
+}
