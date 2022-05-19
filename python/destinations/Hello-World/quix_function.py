273c0411f10301de88567d4a347f25f6dc2aa5b8
Tomas Neubauer <tomas@quix.ai>
2022-05-19 14:53:38 +0200

    Quix function consumer group fix + bugfix

--- python/destinations/Hello-World/quix_function.py
+++ python/destinations/Hello-World/quix_function.py
@@ -4,7 +4,7 @@ from quixstreaming import ParameterData
 class QuixFunction:
 
     # Callback triggered for each new parameter data.
-    def on_parameter_data_handler(data: ParameterData):
+    def on_parameter_data_handler(self, data: ParameterData):
 
         # print first value of ParameterA parameter if it exists
         value = data.timestamps[0].parameters['ParameterA'].numeric_value
