1092a5895f4d2bd8de9e48bec071939c50d16210
tomas-quix <78492422+tomas-quix@users.noreply.github.com>
2022-07-14 10:59:31 +0200

    Feature/realtime dash impr (#226)
    * Realtime dashboard improvements
    
    * readme

--- python/destinations/Realtime-Dashboard/in_memory_view.py
+++ python/destinations/Realtime-Dashboard/in_memory_view.py
@@ -8,9 +8,6 @@ class InMemoryView(CrossStreamStatefullProcessing):
     def __init__(self, input_topic: InputTopic):
         super().__init__(input_topic)
 
-        self.group_by = os.environ["group_by"]
-
-
     def init_state(self):
         self.set_state(pd.DataFrame())
 
@@ -18,12 +15,7 @@ class InMemoryView(CrossStreamStatefullProcessing):
     # Callback triggered for each new parameter data.
     def on_pandas_frame_handler(self, data_df: pd.DataFrame):
 
-        df = pd.concat([self.state, data_df]) \
-            .groupby("TAG__" + self.group_by) \
-            .last() \
-            .reset_index()
-
-        print(df)
+        print(data_df)
         
-        self.set_state(df)
+        self.set_state(data_df)
         
