99b783022660b234aaabdd5cfc47146d491a3bc4
Steve R <steve@quix.ai>
2022-07-22 14:44:34 +0100

    Fix for error

--- python/sources/AmazonKinesis/quix_functions.py
+++ python/sources/AmazonKinesis/quix_functions.py
@@ -1,23 +1,18 @@
 from quixstreaming import StreamWriter, EventData
 from datetime import datetime
-
+import traceback
 
 class QuixFunctions:
-
-    is_connected = False
 
     def __init__(self, stream_writer: StreamWriter):
         self.stream_writer = stream_writer
 
     def write_data(self, data) -> None:
-
+            
         print("Sending RAW data event to Quix")
 
-        event_data = EventData(event_id="raw_data", time=datetime.utcnow(), value=str(data))
-
-        self.stream_writer.events.write(event_data)
-
-        if not self.is_connected:
-            print("CONNECTED!")
-            self.is_connected = True
-
+        try:
+            event_data = EventData(event_id="raw_data", time=datetime.utcnow(), value=str(data))
+            self.stream_writer.events.write(event_data)
+        except BaseException:
+            print(traceback.format_exc())
