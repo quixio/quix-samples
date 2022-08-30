7b9be7e7a06d4453bc15edf9a46de3a3624b034a
Tomas Neubauer <tomas@quix.ai>
2022-08-30 19:40:09 +0200

    Sentiment analysis

--- python/transformations/Sentiment-Analysis/main.py
+++ python/transformations/Sentiment-Analysis/main.py
@@ -1,42 +1,43 @@
-from quixstreaming import QuixStreamingClient, StreamEndType, StreamReader
+from quixstreaming import QuixStreamingClient, StreamEndType, StreamReader, AutoOffsetReset
 from quixstreaming.app import App
-from hugging_face_model import HuggingFaceModel
-from transformers import pipeline
+from quix_function import QuixFunction
 import os
+from transformers import pipeline
+
+
+input_label = 'tweet-text'
+classifier = pipeline('sentiment-analysis')
 
 # Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
 client = QuixStreamingClient()
 
-# Download the Hugging Face model (list of available models here: https://huggingface.co/models)
-model_name = os.environ["HuggingFaceModel"]
-print("Downloading {0} model...".format(model_name))
-model_pipeline = pipeline(model=model_name)
-
-print("Opening input and output topics")
 # Change consumer group to a different constant if you want to run model locally.
-input_topic = client.open_input_topic(os.environ["input"], "default-consumer-group")
+print("Opening input and output topics")
+
+input_topic = client.open_input_topic(os.environ["input"], auto_offset_reset=AutoOffsetReset.Latest)
 output_topic = client.open_output_topic(os.environ["output"])
+
 
 # Callback called for each incoming stream
 def read_stream(input_stream: StreamReader):
+
     # Create a new stream to output data
-    output_stream = output_topic.create_stream(input_stream.stream_id + '-output')
+    output_stream = output_topic.create_stream(input_stream.stream_id)
     output_stream.properties.parents.append(input_stream.stream_id)
 
     # handle the data in a function to simplify the example
-    hugging_face_model = HuggingFaceModel(model_pipeline, input_stream, output_stream)
-
+    quix_function = QuixFunction(input_stream, output_stream, classifier)
+        
     # React to new data received from input topic.
-    input_stream.events.on_read += hugging_face_model.on_event_data_handler
-    input_stream.parameters.on_read += hugging_face_model.on_parameter_data_handler
+    input_stream.events.on_read += quix_function.on_event_data_handler
+    input_topic.on_committed += quix_function.on_committed
 
-    # When input stream closes, we close output stream as well.
-    def on_stream_close(end_type: StreamEndType):
+    # When input stream closes, we close output stream as well. 
+    def on_stream_close(endType: StreamEndType):
         output_stream.close()
         print("Stream closed:" + output_stream.stream_id)
 
     input_stream.on_stream_closed += on_stream_close
-
 
 # Hook up events before initiating read to avoid losing out on any data
 input_topic.on_stream_received += read_stream
