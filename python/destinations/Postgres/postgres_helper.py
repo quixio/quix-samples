48ea8751de672f2fd3177a8d71181128943aa629
senyaoh <senyao.hou@gmail.com>
2022-10-24 21:18:41 +0200

    Get postgres constants from environment variables

--- python/destinations/Postgres/postgres_helper.py
+++ python/destinations/Postgres/postgres_helper.py
@@ -1,11 +1,12 @@
 import psycopg2
+import os
 
 # Postgres Constants
-PG_HOST = ''
-PG_PORT = ''
-PG_USER = ''
-PG_PASSWORD = ''
-PG_DATABASE = ''
+PG_HOST = os.environ["PG_HOST"]
+PG_PORT = os.environ["PG_PORT"]
+PG_USER = os.environ["PG_USER"]
+PG_PASSWORD = os.environ["PG_PASSWORD"]
+PG_DATABASE = os.environ["PG_DATABASE"]
 
 
 class Null:
