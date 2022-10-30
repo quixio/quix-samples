0e11853af365e8bbb4e967972049771d3711195f
senyaoh <senyao.hou@gmail.com>
2022-10-30 20:28:56 +0100

    Added timescale destination

---
+++ python/destinations/Timescale/main.py
@@ -0,0 +1,92 @@
+from quixstreaming import QuixStreamingClient, StreamReader
+from quixstreaming.models.parametersbufferconfiguration import ParametersBufferConfiguration
+from quixstreaming.app import App
+from quix_function import QuixFunction
+import os
+from setup_logger import logger
+from queue import Queue
+from threading import Thread
+from queue_helper import insert_from_queue
+from timescale_helper import connect_timescale, create_paramdata_table, create_metadata_table, create_eventdata_table, create_properties_table, create_parents_table
+
+
+# Global Variables
+TOPIC = os.environ["input"].replace('-', '_')
+TABLE_NAME = {
+    "PARAMETER_TABLE_NAME": TOPIC + '_parameter_data',
+    "EVENT_TABLE_NAME": TOPIC + '_event_data',
+    "METADATA_TABLE_NAME": TOPIC + '_streams_metadata',
+    "PROPERTIES_TABLE_NAME": TOPIC + '_streams_properties',
+    "PARENTS_TABLE_NAME": TOPIC + '_streams_parents'
+}
+
+
+# Connect to Timescale and set up table
+try:
+    conn = connect_timescale()
+    logger.info("CONNECTED!")
+except:
+    # End program or something
+    pass
+
+# Creata table if it doesn't exist
+create_paramdata_table(conn, TABLE_NAME["PARAMETER_TABLE_NAME"])
+create_metadata_table(conn, TABLE_NAME["METADATA_TABLE_NAME"])
+create_eventdata_table(conn, TABLE_NAME["EVENT_TABLE_NAME"])
+create_parents_table(conn, TABLE_NAME["PARENTS_TABLE_NAME"])
+create_properties_table(conn, TABLE_NAME["PROPERTIES_TABLE_NAME"])
+
+
+# Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
+client = QuixStreamingClient()
+
+logger.info("Opening input topic")
+input_topic = client.open_input_topic(os.environ["input"])
+logger.info(os.environ["input"])
+
+# Initialize Queue
+insert_queue = Queue(maxsize=0)
+
+# Create threads that execute insert from Queue
+NUM_THREADS = 10
+WAIT_INTERVAL = 0.25 # Seconds
+BATCH_SIZE = 50
+
+for i in range(NUM_THREADS):
+    worker = Thread(target=insert_from_queue, args=(
+        conn, TABLE_NAME["PARAMETER_TABLE_NAME"], insert_queue, WAIT_INTERVAL, BATCH_SIZE))
+    # Thread will be killed when main thread is terminated
+    worker.setDaemon(True)
+    worker.start()
+
+
+# read streams
+def read_stream(input_stream: StreamReader):
+    logger.info("New stream read:" + input_stream.stream_id)
+
+    buffer_options = ParametersBufferConfiguration()
+    buffer_options.time_span_in_milliseconds = 100
+
+    buffer = input_stream.parameters.create_buffer(buffer_options)
+
+    # handle the data in a function to simplify the example
+    quix_function = QuixFunction(conn, TABLE_NAME, insert_queue, input_stream)
+
+    buffer.on_read += quix_function.on_parameter_data_handler
+    input_stream.events.on_read += quix_function.on_event_data_handler
+
+    input_stream.properties.on_changed += quix_function.on_stream_properties_changed
+    input_stream.on_stream_closed += quix_function.on_stream_closed
+    input_stream.parameters.on_definitions_changed += quix_function.on_parameter_definition_changed
+
+    input_topic.on_committing += quix_function.on_committing
+
+
+# Hook up events before initiating read to avoid losing out on any data
+input_topic.on_stream_received += read_stream
+
+# Hook up to termination signal (for docker image) and CTRL-C
+logger.info("Listening to streams. Press CTRL-C to exit.")
+
+# Handle graceful exit
+App.run()
---
+++ python/destinations/Timescale/quix_function.py
@@ -0,0 +1,128 @@
+from quixstreaming import ParameterData, EventData, StreamEndType, StreamReader
+
+import os
+from queue import Queue
+from datetime import datetime
+from setup_logger import logger
+from timescale_helper import create_column, insert_row, delete_row, Null
+
+class QuixFunction:
+
+    def __init__(self, conn, table_name, insert_queue: Queue, input_stream: StreamReader):
+        self.conn = conn
+        self.table_name = table_name
+        self.insert_queue = insert_queue
+        self.input_stream = input_stream
+        self.data_start = Null()
+        self.data_end = Null()
+        self.insert_parents()
+        self.topic = os.environ["input"].replace('-', '_')
+        self.committing = False
+
+    def on_committing(self):
+        logger.debug("on_committing")
+        self.committing = True
+
+    def reset_data_ts(self):
+        self.data_start = Null()
+        self.data_end = Null()
+
+    def format_nanoseconds(self, nanos):
+        dt = datetime.fromtimestamp(nanos / 1e9)
+        return '{}.{:09.0f}'.format(dt.strftime('%Y-%m-%dT%H:%M:%S'), nanos % 1e9)
+
+    # Callback triggered for each new parameter data.
+    def on_parameter_data_handler(self, data: ParameterData):
+
+        for ts in data.timestamps:
+            row = {'timestamp': self.format_nanoseconds(ts.timestamp_nanoseconds)}
+            if type(self.data_start) == Null:
+                self.data_start = ts.timestamp_nanoseconds
+                self.data_end = ts.timestamp_nanoseconds
+            self.data_start = min(self.data_start, ts.timestamp_nanoseconds)
+            self.data_end = max(self.data_end, ts.timestamp_nanoseconds)
+
+            for k, v in ts.tags.items():
+                row['TAG_' + k] = v
+
+            for k, v in ts.parameters.items():
+                if v.numeric_value:
+                    row[k + '_n'] = v.numeric_value
+
+                if v.string_value:
+                    row[k + '_s'] = v.string_value
+
+            # Add to Queue
+            if not self.committing:
+                self.insert_queue.put(row, block=True)
+
+            if self.insert_queue.qsize() == 0 and self.committing == True:
+                self.committing = False
+                logger.debug("Commit success!")
+
+    def insert_metadata(self):
+        cols = []
+        vals = []
+        for k, v in self.input_stream.properties.metadata.items():
+            create_column(
+                self.conn, self.table_name["METADATA_TABLE_NAME"], k, 'STRING')
+            cols.append(k)
+            vals.append(v)
+        insert_row(
+            self.conn, self.table_name["METADATA_TABLE_NAME"], cols, [vals])
+
+    def insert_parents(self):
+        for parent in self.input_stream.properties.parents:
+            cols = ['stream_id', 'parent_id']
+            vals = [self.input_stream.stream_id, parent]
+            insert_row(
+                self.conn, self.table_name["PARENTS_TABLE_NAME"], cols, [vals])
+
+    def update_parents(self):
+        delete_row(self.conn, self.table_name["PARENTS_TABLE_NAME"],
+                   f"stream_id = '{self.input_stream.stream_id}'")
+        self.insert_parents()
+
+    def insert_properties(self, status: str):
+        cols = ["name", "location", "topic",
+                "status", "data_start", "data_end"]
+        vals = [self.input_stream.properties.name, self.input_stream.properties.location,
+                self.topic, status, self.data_start, self.data_end]
+        insert_row(
+            self.conn, self.table_name["PROPERTIES_TABLE_NAME"], cols, [vals])
+
+    def on_event_data_handler(self, data: EventData):
+        logger.debug("on_event_data_handler")
+
+        row = {'timestamp': self.format_nanoseconds(data.timestamp_nanoseconds)}
+
+        for k, v in data.tags.items():
+            create_column(
+                self.conn, self.table_name["EVENT_TABLE_NAME"], 'TAG_' + k, 'STRING')
+            row['TAG_' + k] = v
+
+        row['value'] = data.value
+        insert_row(self.conn, self.table_name["EVENT_TABLE_NAME"], list(
+            row.keys()), [list(row.values())])
+
+    def on_stream_properties_changed(self):
+        logger.debug("on_stream_properties_changed")
+        self.insert_metadata()
+        self.insert_properties("open")
+        # Reset data start and end
+        self.reset_data_ts()
+
+        self.update_parents()
+
+    def on_parameter_definition_changed(self):
+        logger.debug("on_parameter_definition_changed")
+        self.insert_metadata()
+        self.insert_properties("open")
+        # Reset data start and end
+        self.reset_data_ts()
+
+        self.update_parents()
+
+    def on_stream_closed(self, data: StreamEndType):
+        logger.debug("on_stream_closed")
+        self.insert_properties(str(data))
