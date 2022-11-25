621ef53f6f4b61da9accb9796ad680975fb22338
Senyao Hou <31976816+senyaoh@users.noreply.github.com>
2022-11-25 14:55:22 +0100

    Added Postgres CDC source (#281)
    * Added postgres CDC source
    
    * Set schema and table name in env var
    
    * Fix global variables

---
+++ python/sources/Postgres-CDC/main.py
@@ -0,0 +1,52 @@
+from quixstreaming import QuixStreamingClient
+import time
+import datetime
+import os
+import json
+from setup_logger import logger
+from postgres_helper import connect_postgres, create_logical_slot, create_publication_on_table, get_changes
+
+#Global Variables
+PG_SLOT_NAME = "replication_slot"
+PG_SCHEMA = os.environ["PG_SCHEMA"]
+PG_TABLE = os.environ["PG_TABLE"]
+PG_PUBLICATION_NAME = f"pub_{PG_SCHEMA}_{PG_TABLE}"
+PG_TABLE_NAME = f"{PG_SCHEMA}.{PG_TABLE}"
+WAIT_INTERVAL = 0.1
+# Connect to postgres and set up table
+try:
+    create_logical_slot(PG_SLOT_NAME)
+    create_publication_on_table(PG_PUBLICATION_NAME, PG_TABLE_NAME)
+    conn = connect_postgres()
+except:
+    # End program or something
+    pass
+
+
+# Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
+client = QuixStreamingClient()
+
+# Open the output topic where to write data out
+output_topic = client.open_output_topic(os.environ["output"])
+
+stream = output_topic.create_stream()
+stream.properties.name = f"{PG_TABLE_NAME} CDC"
+stream.parameters.add_definition("cdc_data")
+stream.parameters.buffer.time_span_in_milliseconds = 100
+
+logger.info(f"Start stream CDC for table: {PG_TABLE_NAME}")
+while True:
+    records = get_changes(conn, PG_SLOT_NAME)
+    for record in records:
+        changes = json.loads(record[0])
+        for change in changes["change"]:
+            logger.debug(json.dumps(change))
+            stream.parameters \
+                .buffer \
+                .add_timestamp(datetime.datetime.utcnow()) \
+                .add_value("cdc_data", json.dumps(change)) \
+                .write()
+    time.sleep(WAIT_INTERVAL)
+
+logger.info("Closing stream")
+stream.close()
---
+++ python/sources/Postgres-CDC/postgres_helper.py
@@ -0,0 +1,73 @@
+import psycopg2
+import os
+# Postgres Constants
+PG_HOST = os.environ["PG_HOST"]
+PG_PORT = os.environ["PG_PORT"]
+PG_USER = os.environ["PG_USER"]
+PG_PASSWORD = os.environ["PG_PASSWORD"]
+PG_DATABASE = os.environ["PG_DATABASE"]
+
+def connect_postgres():
+    conn = psycopg2.connect(
+        database=PG_DATABASE, user=PG_USER, password=PG_PASSWORD, host=PG_HOST, port=PG_PORT
+    )
+    return conn
+
+
+def run_query(conn, query: str):
+    cur = conn.cursor()
+    cur.execute(query)
+    conn.commit()
+    cur.close()
+
+
+def create_logical_slot(slot_name: str):
+    conn = connect_postgres()
+    query = f'''
+    SELECT pg_create_logical_replication_slot('{slot_name}', 'wal2json');
+    '''
+    try:
+        run_query(conn, query)
+        conn.close()
+
+    except psycopg2.errors.DuplicateObject:
+        print(f"Replication slot {slot_name} already exists.")
+        conn.close()
+
+    else:
+        conn.close()
+        raise
+
+
+def create_publication_on_table(publication_name: str, table_name: str):
+    conn = connect_postgres()
+    query = f'''
+    CREATE PUBLICATION {publication_name} FOR TABLE {table_name};
+    '''
+    try:
+        run_query(conn, query)
+        conn.close()
+
+    except psycopg2.errors.DuplicateObject:
+        print(f"Publication {publication_name} already exists.")
+        conn.close()
+        
+    except psycopg2.errors.UndefinedTable:
+        print(f"{table_name} not found.")
+        conn.close()
+        raise
+
+    else:
+        conn.close()
+        raise
+
+def get_changes(conn, slot_name: str):
+    query = f'''
+    SELECT data FROM pg_logical_slot_get_changes('{slot_name}', NULL, NULL);
+    '''
+    cur = conn.cursor()
+    cur.execute(query)
+    records = cur.fetchall()
+    cur.close()
+    return records
+
