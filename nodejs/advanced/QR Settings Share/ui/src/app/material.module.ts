dfd5b6d524a58a88131003e7b4d72e6252761c25
SteveRosam <1799158+srosam@users.noreply.github.com>
2023-01-20 11:38:23 +0000

    one

---
+++ nodejs/advanced/QR Settings Share/ui/src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ]
+})
+export class MaterialModule { }
