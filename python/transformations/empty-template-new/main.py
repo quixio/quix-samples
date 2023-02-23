9ebd3929603ed8ed3b72518d614629a4ffff322b
Tomas Neubauer <tomas@quix.ai>
2023-02-23 16:42:44 +0100

    Preview of transformation using new open source library QuixStreams

---
+++ python/transformations/empty-template-new/main.py
@@ -0,0 +1,20 @@
+from quixstreams import QuixStreamingClient, StreamConsumer
+from quixstreams.app import App
+import os
+import pandas as pd
+
+client = QuixStreamingClient()
+topic_consumer = client.create_topic_consumer(os.environ["input"], "sum")
+topic_producer = client.create_topic_producer(os.environ["output"])
+
+def on_dataframe_receive(stream_consumer: StreamConsumer, df: pd.DataFrame):
+    print(df)
+
+def read_stream(stream_consumer: StreamConsumer):
+    stream_consumer.timeseries.on_dataframe_receive = on_dataframe_receive
+       
+topic_consumer.on_stream_received = read_stream  # we subscribe to data for each stream.
+
+print("Listening to streams. Press CTRL-C to exit.")
+
+App.run() # Handle graceful exit
