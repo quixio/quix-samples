42ca049a3e19c590e05a135fa20960b7f594e6ba
SteveRosam <steve@quix.io>
2023-03-07 13:30:24 +0000

    Use QuixStreams

--- csharp/sources/empty-template/WriteHelloWorld/Program.cs
+++ csharp/sources/empty-template/WriteHelloWorld/Program.cs
@@ -1,26 +1,26 @@
 ﻿// Create a client that holds generic details for creating input and output topics
-var client = new Quix.Sdk.Streaming.QuixStreamingClient();
+var client = new QuixStreams.Streaming.QuixStreamingClient();
 
 var outputTopicName = Environment.GetEnvironmentVariable("output");
             
-using var outputTopic = client.OpenOutputTopic(outputTopicName);
+using var outputTopic = client.GetTopicProducer(outputTopicName);
             
 Console.WriteLine("Creating stream");
 var stream = outputTopic.CreateStream();
 
 stream.Properties.Name = "Hello World stream";
            
-stream.Parameters
+stream.Timeseries
     .AddDefinition("ParameterA")
     .SetRange(-1.2, 1.2);
            
 Console.WriteLine("Sending values for 30 seconds");
 for (var index = 0; index < 3000; index++)
 {
-    stream.Parameters.Buffer
+    stream.Timeseries.Buffer
         .AddTimestamp(DateTime.UtcNow)
         .AddValue("ParameterA", Math.Sin(index/200.0) + Math.Sin(index) / 5.0)
-        .Write(); 
+        .Publish(); 
                     
     Thread.Sleep(10);
 }
