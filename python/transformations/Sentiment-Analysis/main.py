9ce4c46294392b47dfea03a60d811d2ef4e7154e
SteveRosam <steve@quix.io>
2023-03-14 15:09:02 +0000

    Update Sentiment analysis example to use QuixStreams
    Also spotted an issue with downsampling project json file

--- python/transformations/Sentiment-Analysis/main.py
+++ python/transformations/Sentiment-Analysis/main.py
@@ -1,48 +1,47 @@
-from quixstreaming import QuixStreamingClient, StreamEndType, StreamReader, AutoOffsetReset
-from quixstreaming.app import App
+import quixstreams as qx
 from quix_function import QuixFunction
-import os
 from transformers import pipeline
+import os
 
 classifier = pipeline('sentiment-analysis')
 
-
-# Quix injects credentials automatically to the client. Alternatively, you can always pass an SDK token manually as an argument.
-client = QuixStreamingClient()
+# Quix injects credentials automatically to the client.
+# Alternatively, you can always pass an SDK token manually as an argument.
+client = qx.QuixStreamingClient()
 
 # Change consumer group to a different constant if you want to run model locally.
 print("Opening input and output topics")
 
-input_topic = client.open_input_topic(os.environ["input"], auto_offset_reset=AutoOffsetReset.Latest)
-output_topic = client.open_output_topic(os.environ["output"])
+consumer_topic = client.get_topic_consumer(os.environ["input"], auto_offset_reset=qx.AutoOffsetReset.Latest)
+producer_topic = client.get_topic_producer(os.environ["output"])
 
 
 # Callback called for each incoming stream
-def read_stream(input_stream: StreamReader):
+def read_stream(consumer_stream: qx.StreamConsumer):
 
     # Create a new stream to output data
-    output_stream = output_topic.create_stream(input_stream.stream_id)
-    output_stream.properties.parents.append(input_stream.stream_id)
+    producer_stream = producer_topic.get_or_create_stream(consumer_stream.stream_id)
+    producer_stream.properties.parents.append(consumer_stream.stream_id)
 
     # handle the data in a function to simplify the example
-    quix_function = QuixFunction(input_stream, output_stream, classifier)
+    quix_function = QuixFunction(consumer_stream, producer_stream, classifier)
 
     # React to new data received from input topic.
-    input_stream.events.on_read += quix_function.on_event_data_handler
-    input_stream.parameters.on_read_pandas += quix_function.on_pandas_frame_handler
+    consumer_stream.events.on_data_received = quix_function.on_event_data_handler
+    consumer_stream.timeseries.on_dataframe_received = quix_function.on_pandas_frame_handler
 
     # When input stream closes, we close output stream as well. 
-    def on_stream_close(endType: StreamEndType):
-        output_stream.close()
-        print("Stream closed:" + output_stream.stream_id)
+    def on_stream_close():
+        producer_stream.close()
+        print("Stream closed:" + producer_stream.stream_id)
 
-    input_stream.on_stream_closed += on_stream_close
+    consumer_stream.on_stream_closed = on_stream_close
 
-# Hook up events before initiating read to avoid losing out on any data
-input_topic.on_stream_received += read_stream
 
-# Hook up to termination signal (for docker image) and CTRL-C
+# Subscribe to events before initiating read to avoid losing out on any data
+consumer_topic.on_stream_received = read_stream
+
 print("Listening to streams. Press CTRL-C to exit.")
 
 # Handle graceful exit of the model.
-App.run()
+qx.App.run()
