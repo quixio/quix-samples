22778c102826fd4d76cea9122ee670d18fcd8b8c
SteveRosam <steve@quix.io>
2023-03-14 10:31:45 +0000

    Update to ensure new streams don't break stuff

--- python/transformations/DownSampling/main.py
+++ python/transformations/DownSampling/main.py
@@ -5,10 +5,9 @@ import os
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
-stream_producer: qx.StreamProducer = None
 
 print("Opening input and output topics")
-topic_consumer = client.get_topic_consumer(os.environ["input"], "default-consumer-group2")
+topic_consumer = client.get_topic_consumer(os.environ["input"], "down-sampling-consumer-group")
 topic_producer = client.get_topic_producer(os.environ["output"])
 
 # buffer 100ms of data
@@ -16,33 +15,32 @@ buffer_configuration = qx.TimeseriesBufferConfiguration()
 buffer_configuration.time_span_in_milliseconds = 100
 
 
-def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-
-    # look for the timestamp column
-    # if yours is named differently please change this code as needed
-    if "timestamp" in df:
-        df["date_time"] = pd.to_datetime(df["timestamp"])
-    elif "time" in df:
-        df["date_time"] = pd.to_datetime(df["time"])
-    else:
-        raise Exception("A suitable timestamp was column not found in the dataset")
-
-    # this sample uses 100ms of data and down-samples to 10ms
-    td = pd.Timedelta(10, "milliseconds")
-
-    # resample and get the mean of the input data
-    df = df.set_index("date_time").resample(td).mean().ffill()
-    print(df)
-
-    # Send filtered data to output topic
-    stream_producer.timeseries.buffer.publish(df)
-
-
-# Callback called for each incoming stream
+# called for each incoming stream
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
-    global stream_producer
 
-    # Create a new stream to output data
+    # called for each incoming DataFrame
+    def on_dataframe_received_handler(originating_stream: qx.StreamConsumer, df: pd.DataFrame):
+
+        # look for the timestamp column
+        # if yours is named differently please change this code as needed
+        if "timestamp" in df:
+            df["date_time"] = pd.to_datetime(df["timestamp"])
+        elif "time" in df:
+            df["date_time"] = pd.to_datetime(df["time"])
+        else:
+            raise Exception("A suitable timestamp was column not found in the dataset")
+
+        # this sample uses 100ms of data and down-samples to 10ms
+        td = pd.Timedelta(10, "milliseconds")
+
+        # resample and get the mean of the input data
+        df = df.set_index("date_time").resample(td).mean().ffill()
+        print(df)
+
+        # Send filtered data to output topic
+        stream_producer.timeseries.buffer.publish(df)
+
+    # create a new stream to output data
     stream_producer = topic_producer.get_or_create_stream(stream_consumer.stream_id + "-down-sampled")
     stream_producer.properties.parents.append(stream_consumer.stream_id)
 
