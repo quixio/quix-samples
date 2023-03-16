9b7fd22baa485804126ccabf60a2a27903d2573c
SteveRosam <steve@quix.io>
2023-03-16 10:35:58 +0000

    Update to use QuixStreams

--- python/transformations/Event-Detection/quix_function.py
+++ python/transformations/Event-Detection/quix_function.py
@@ -1,34 +1,35 @@
-from quixstreaming import StreamReader, StreamWriter, EventData, ParameterData
+import quixstreams as qx
 import pandas as pd
 
 
 class QuixFunction:
-    def __init__(self, input_stream: StreamReader, output_stream: StreamWriter):
-        self.input_stream = input_stream
-        self.output_stream = output_stream
+    def __init__(self, consumer_stream: qx.StreamConsumer, producer_stream: qx.StreamProducer):
+        self.consumer_stream = consumer_stream
+        self.producer_stream = producer_stream
 
-    # Callback triggered for each new event.
-    def on_event_data_handler(self, data: EventData):
+    # Callback triggered for each new event
+    def on_event_data_handler(self, stream_consumer: qx.StreamConsumer, data: qx.EventData):
         print(data.value)
 
-        # Here transform your data.
+        # Transform your data here.
 
-        self.output_stream.events.write(data)
+        self.producer_stream.events.publish(data)
 
-    # Callback triggered for each new parameter data.
-    def on_pandas_frame_handler(self, df: pd.DataFrame):
+    # Callback triggered for each new timeseries data
+    def on_pandas_frame_handler(self, stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
         
         output_df = pd.DataFrame()
-        output_df["time"] = df["time"]
+        output_df["timestamp"] = df["timestamp"]
 
         if "TAG__LapNumber" in df.columns:
             output_df["TAG__LapNumber"] = df["TAG__LapNumber"]
             
         print(df)
 
-        # If braking force applied is more than 50%, we send True.  
+        # If braking force applied is more than 50%, we send True.
+        # update this code to apply your own logic or ML model processing
         if "Brake" in df.columns:
             output_df["HardBraking"] = df.apply(lambda row: "True" if row.Brake > 0.5 else "False", axis=1)  
 
-        self.output_stream.parameters.buffer.write(output_df)  # Send filtered data to output topic
+        self.producer_stream.timeseries.buffer.publish(output_df)
 
