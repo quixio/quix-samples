fc77e856160b705f758f4d44cdf641d9849d311a
Steve <100689438+SteveRosam@users.noreply.github.com>
2023-03-22 13:46:52 +0000

    Update to use QuixStreams (#361)
    Update Threshold Alert to use QuixStreams

--- python/transformations/Threshold-Alert/main.py
+++ python/transformations/Threshold-Alert/main.py
@@ -1,5 +1,4 @@
-from quixstreaming import QuixStreamingClient, StreamEndType, StreamReader, ParametersBufferConfiguration
-from quixstreaming.app import App
+import quixstreams as qx
 from threshold_function import ThresholdAlert
 import os
 import traceback
@@ -7,14 +6,14 @@ import traceback
 
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
-client = QuixStreamingClient()
+client = qx.QuixStreamingClient()
 
 # Change consumer group to a different constant if you want to run model locally.
 print("Opening input and output topics")
 
 # Environment variables
-input_topic = client.open_input_topic(os.environ["input"], "default-consumer-group")
-output_topic = client.open_output_topic(os.environ["output"])
+consumer_topic = client.get_topic_consumer(os.environ["input"], "default-consumer-group")
+producer_topic = client.get_topic_producer(os.environ["output"])
 
 try:
     bufferMilliSeconds = os.environ["bufferMilliSeconds"]
@@ -24,34 +23,34 @@ except ValueError:
 
 
 # Callback called for each incoming stream
-def read_stream(input_stream: StreamReader):
+def read_stream(consumer_stream: qx.StreamConsumer):
     # Create a new stream to output data
-    output_stream = output_topic.create_stream(input_stream.stream_id + '-' + os.environ["Quix__Deployment__Name"])
-    output_stream.properties.parents.append(input_stream.stream_id)
+    producer_stream = producer_topic.get_or_create_stream(consumer_stream.stream_id + '-' + os.environ["Quix__Deployment__Name"])
+    producer_stream.properties.parents.append(consumer_stream.stream_id)
 
     # handle the data in a function to simplify the example
-    quix_function = ThresholdAlert(input_stream, output_stream)
+    quix_function = ThresholdAlert(producer_stream)
 
-    buffer_options = ParametersBufferConfiguration()
+    buffer_options = qx.TimeseriesBufferConfiguration()
     buffer_options.time_span_in_milliseconds = msecs
-    buffer = input_stream.parameters.create_buffer()
+    buffer = consumer_stream.timeseries.create_buffer()
 
     # React to new data received from input topic.
-    buffer.on_read_pandas += quix_function.on_pandas_frame_handler
+    buffer.on_dataframe_released = quix_function.on_pandas_frame_handler
 
     # When input stream closes, we close output stream as well.
-    def on_stream_close(end_type: StreamEndType):
-        output_stream.close()
-        print("Stream closed:" + output_stream.stream_id)
+    def on_stream_close(stream_consumer: qx.StreamConsumer, end_type: qx.StreamEndType):
+        producer_stream.close()
+        print("Stream closed:" + producer_stream.stream_id)
 
-    input_stream.on_stream_closed += on_stream_close
+    consumer_stream.on_stream_closed = on_stream_close
 
 
 # Hook up events before initiating read to avoid losing out on any data
-input_topic.on_stream_received += read_stream
+consumer_topic.on_stream_received = read_stream
 
 # Hook up to termination signal (for docker image) and CTRL-C
 print("Listening to streams. Press CTRL-C to exit.")
 
 # Handle graceful exit of the model.
-App.run()
+qx.App.run()
