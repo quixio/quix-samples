8b2aeaedda3b140d43a34d376403316f53fe0c6f
JotaBlanco <50139826+JotaBlanco@users.noreply.github.com>
2023-03-29 14:46:50 +0200

    Threshold Sample Update
    Now operates both with buffers and single messages, also uses quixstreams

--- python/transformations/Percentage-Alert/main.py
+++ python/transformations/Percentage-Alert/main.py
@@ -2,7 +2,7 @@ import quixstreams as qx
 from percentage_function import PercentageAlert
 import os
 
-# Quix injects credentials automatically to the client.
+
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
 
@@ -10,33 +10,34 @@ client = qx.QuixStreamingClient()
 print("Opening input and output topics")
 
 # Define environmental variables
+
 consumer_topic = client.get_topic_consumer(os.environ["input"], "default-consumer-group")
 producer_topic = client.get_topic_producer(os.environ["output"])
 
 
-# Callback called for each incoming stream
-def read_stream(stream_consumer: qx.StreamConsumer):
+def consume_stream(consumer_stream: qx.StreamConsumer):
     # Create a new stream to output data
-    stream_producer = producer_topic.create_stream(stream_consumer.stream_id + '-' + os.environ["Quix__Deployment__Name"])
-    stream_producer.properties.parents.append(stream_consumer.stream_id)
+
+    producer_stream = producer_topic.get_or_create_stream(consumer_stream.stream_id + '-' + os.environ["Quix__Deployment__Name"])
+    producer_stream.properties.parents.append(consumer_stream.stream_id)
 
     # handle the data in a function to simplify the example
-    quix_function = PercentageAlert(stream_consumer, stream_producer)
+    perc_alert = PercentageAlert(consumer_stream, producer_stream)
 
     # React to new data received from input topic.
-    stream_consumer.events.on_data_received = quix_function.on_event_data_handler
-    stream_consumer.timeseries.on_dataframe_received = quix_function.on_dataframe_handler
+    consumer_stream.timeseries.on_dataframe_received = perc_alert.on_data_frame_handler
 
-    # When input stream closes, we close output stream as well. 
-    def on_stream_close(stream_consumer: qx.StreamConsumer, end_type: qx.StreamEndType):
-        stream_producer.close()
-        print("Stream closed:" + stream_producer.stream_id)
 
-    stream_consumer.on_stream_closed = on_stream_close
+    def on_stream_close():
+        producer_stream.close()
+        print("Stream closed:" + producer_stream.stream_id)
+
+    consumer_stream.on_stream_closed = on_stream_close
 
 
 # Hook up events before initiating read to avoid losing out on any data
-consumer_topic.on_stream_received = read_stream
+
+consumer_topic.on_stream_received = consume_stream
 
 # Hook up to termination signal (for docker image) and CTRL-C
 print("Listening to streams. Press CTRL-C to exit.")
