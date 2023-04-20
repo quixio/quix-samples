4186c882a7024e775a25c45299bcd5dd4860aa6a
SteveRosam <steve@quix.io>
2023-04-20 14:07:33 +0100

    Temporarily revert Confluent Kafka connector to use QuixStreaming

--- python/destinations/ConfluentKafka/quix_function.py
+++ python/destinations/ConfluentKafka/quix_function.py
@@ -1,14 +1,17 @@
-import quixstreams as qx
 import json
+
+from quixstreaming import StreamReader
+from quixstreaming.models.streampackage import StreamPackage
+from quixstreaming.raw import RawOutputTopic, RawMessage
 
 
 class QuixFunctions:
     topic_root = 'not_set'
 
-    def __init__(self, stream_producer: qx.RawTopicProducer):
-        self.stream_producer = stream_producer
+    def __init__(self, output_stream: RawOutputTopic):
+        self.output_stream = output_stream
 
-    def package_received_handler(self, s: qx.StreamConsumer, p: qx.models.streampackage.StreamPackage):
+    def package_received_handler(self, s: StreamReader, p: StreamPackage):
 
         # build the payload object
         payload = {
@@ -21,7 +24,7 @@ class QuixFunctions:
         # convert to a byte array
         json_payload_bytes = bytearray(json_payload, "ansi")
         # create the RawMessage object
-        message = qx.RawMessage(json_payload_bytes)
+        message = RawMessage(json_payload_bytes)
         # publish the json data to Confluent
-        self.stream_producer.publish(message)
+        self.output_stream.write(message)
 
