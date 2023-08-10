327fec8751f78145554cb99384badac5221005f9
SteveRosam <steve@quix.io>
2023-08-10 12:43:05 +0100

    Update S3 Sink

--- python/destinations/S3/main.py
+++ python/destinations/S3/main.py
@@ -7,22 +7,27 @@ from threading import Lock, Thread
 import gzip
 import boto3
 
+headers = ["timestamp"]
+
 # keep the main loop running
 run = True
+
+# comma separated list of parameters that contains formatted data to save to s3
+param_csv = os.environ["parameters"]
+# split and trim the entries
+params_to_write = [x.strip() for x in param_csv.split(',')]
+headers += params_to_write
+
+bucket = os.environ["s3_bucket"]
+s3_folder = os.environ["s3_folder"]
+# if set to true, new folders will be created in s3 for each stream using stream id as name 
+s3_folder_per_stream = os.environ["s3_folder_per_stream"].lower() == "true"
 
 client = qx.QuixStreamingClient()
 commit_settings = qx.models.CommitOptions()
 commit_settings.auto_commit_enabled = False
 topic = client.get_topic_consumer(os.environ["input"], "s3-sink", 
                                   commit_settings = commit_settings, auto_offset_reset = qx.AutoOffsetReset.Latest)
-
-# name of the parameter that contains formatted data to save to s3
-param = os.environ["parameter"]
-bucket = os.environ["s3_bucket"]
-s3_folder = os.environ["s3_folder"]
-# if set to true, new folders will be created in s3 for each stream using stream id as name 
-s3_folder_per_stream = os.environ["s3_folder_per_stream"].lower() == "true"
-separator = os.environ["separator"]
 
 # files created in s3 will have this prefix
 prefix = os.environ["prefix"]
@@ -55,12 +60,17 @@ BatchMode = Enum("BatchMode", "NONE TIME COUNT TIME_OR_COUNT")
 batch_mode = BatchMode.NONE
 if max_count > 0 and max_interval.total_seconds() > 0:
     batch_mode = BatchMode.TIME_OR_COUNT
+    print(f"Files will be uploaded approx. every {max_interval.total_seconds()} seconds or {max_count} records. Whichever is soonest.")
 elif max_count > 0:
     batch_mode = BatchMode.COUNT
+    print(f"Files will be uploaded every {max_count} records")
 elif max_interval.total_seconds() > 0:
     batch_mode = BatchMode.TIME
+    print(f"Files will be uploaded approx. every {max_interval.total_seconds()} seconds.")
 else:
     batch_mode = BatchMode.NONE
+    print("Files will be uploaded for every record. Time and Count parameters will be ignored.")
+
 batches = {}
 
 s3 = boto3.client(
@@ -72,7 +82,6 @@ s3 = boto3.client(
 def upload(stream_id: str, fname: str):
     if os.path.exists(fname):
         try:
-            print("Uploading file")
             if s3_folder:
                 if s3_folder_per_stream:
                     path = "/".join([s3_folder, stream_id, fname])
@@ -89,6 +98,7 @@ def upload(stream_id: str, fname: str):
             print("Error: upload(): " + str(e))
         finally:
             os.remove(fname)
+            headers_written_for_file.remove(fname)
     else:
         print("Error: file " + fname + " not found")
 
@@ -106,9 +116,12 @@ def is_new_batch(batch: Batch):
         return interval >= max_interval or batch.count >= max_count
     raise Exception("Unknown batch mode")
 
+
 # modify this function, if you prefer a different file naming logic.
 def file_name(start: datetime):
     return prefix + start.astimezone(tz).isoformat(timespec='milliseconds').replace(":", "").replace("+", "_plus_") + suffix + ".gz"
+
+headers_written_for_file = []
 
 def save(stream_id: str, data: qx.TimeseriesData):
     global batches
@@ -120,16 +133,38 @@ def save(stream_id: str, data: qx.TimeseriesData):
                 if len(data.timestamps) > 1 and (batch_mode == BatchMode.COUNT or batch_mode == BatchMode.TIME_OR_COUNT):
                     print("Warn: data contains more than one timestamp: batch size may not be accurate")
                 with gzip.open(batch.fname, "at") as fd:
+                    output_data = ""
                     for ts in data.timestamps:
-                        # to save something other than string or numeric data (e.g. binary data), change access to the correct value type
-                        if ts.parameters[param].string_value is not None:
-                            fd.write(ts.parameters[param].string_value)
-                            batch.count += 1
-                            fd.write(separator)
-                        if ts.parameters[param].numeric_value is not None:
-                            fd.write(str(ts.parameters[param].numeric_value))
-                            batch.count += 1
-                            fd.write(separator)
+                        output_rows = ""
+
+                        for f in params_to_write:
+                            comma = ""
+                            if output_rows != "":
+                                comma = ","
+
+                            if f in ts.parameters.keys():
+                                if ts.parameters[f].string_value is not None:
+                                    output_rows += f"{comma}{ts.parameters[f].string_value}"
+                                elif ts.parameters[f].numeric_value is not None:
+                                    output_rows += f"{comma}{ts.parameters[f].numeric_value}"
+                                else:
+                                    output_rows += f"{comma}{0}"
+                            else:
+                                output_rows += f"{comma}{0}"
+
+                        output_rows += "\n"
+                        output_rows = f"{ts.timestamp_nanoseconds},{output_rows}"
+                        output_data += output_rows
+
+                    if output_data != "":
+                        if batch.fname not in headers_written_for_file:
+                            fd.write(",".join(headers))
+                            fd.write("\n")
+                            headers_written_for_file.append(batch.fname)
+
+                        batch.count += 1
+                        fd.write(output_data)
+
                 if is_new_batch(batch):
                     if batch.count > 0:
                         upload(stream_id, batch.fname)
@@ -167,10 +202,9 @@ topic.on_stream_received = stream_received_handler
 def job():
     while run:
         print("Debug: started batch job at " + str(datetime.now()))
-        print(len(batches))
-        for key in batches.keys():
-            mutex.acquire()
-            try:
+        mutex.acquire()
+        try:
+            for key in batches.keys():
                 now = datetime.now()
                 batch = batches[key]
                 if is_new_batch(batch):
@@ -179,8 +213,8 @@ def job():
                         print("Info: job() uploaded batch " + batch.fname + " with " + str(batch.count) + " records to S3")
                     fname = file_name(now)
                     batches[key] = Batch(0, now, fname)
-            finally:
-                mutex.release()
+        finally:
+            mutex.release()
         interval = max_interval.total_seconds() / 2
         print("Debug: scheduled next batch job to run at " + str(datetime.now() + timedelta(seconds = interval)))
         time.sleep(interval)
