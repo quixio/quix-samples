178937b799a4a5948d65b20e6bbaccb4b821dafc
merlin-quix <116729413+merlin-quix@users.noreply.github.com>
2024-02-14 22:09:09 +0700

    converting output connector to SDF

--- python/destinations/InfluxDB/main.py
+++ python/destinations/InfluxDB/main.py
@@ -1,21 +1,20 @@
-import quixstreams as qx
+from quixstreams import Application
+from quixstreams.models.serializers.quix import JSONDeserializer
 import os
-import pandas as pd
 import influxdb_client_3 as InfluxDBClient3
 import ast
 import datetime
+import pandas as pd
 
+app = Application.Quix(consumer_group="influx-destination",
+                       auto_offset_reset="earliest")
 
-client = qx.QuixStreamingClient()
-
-# get the topic consumer for a specific consumer group
-topic_consumer = client.get_topic_consumer(topic_id_or_name = os.environ["input"],
-                                           consumer_group = "empty-destination")
+input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
 
 # Read the environment variable and convert it to a list
 tag_columns = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "[]"))
 
-# Read the enviroment variable for measurement name and convert it to a list
+# Read the environment variable for measurement name and convert it to a list
 measurement_name = os.environ.get('INFLUXDB_MEASUREMENT_NAME', os.environ["input"])
                                            
 client = InfluxDBClient3.InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
@@ -23,36 +22,29 @@ client = InfluxDBClient3.InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
                          org=os.environ["INFLUXDB_ORG"],
                          database=os.environ["INFLUXDB_DATABASE"])
 
-
-def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+def send_data_to_influx(row):
     try:
+        # Convert the dictionary to a DataFrame
+        row_df = pd.DataFrame([row])
+
         # Reformat the dataframe to match the InfluxDB format
-        df = df.rename(columns={'timestamp': 'time'})
-        df = df.set_index('time')
-        df["stream_id"] = stream_consumer.stream_id
+        row_df = row_df.rename(columns={'timestamp': 'time'})
+        row_df = row_df.set_index('time')
 
-        client.write(df, data_frame_measurement_name=measurement_name, data_frame_tag_columns=tag_columns) 
+        client._write_api.write(
+            bucket=os.environ["INFLUXDB_DATABASE"], 
+            record=row_df, 
+            data_frame_measurement_name='conversationstest', 
+            data_frame_tag_columns=tag_columns)
 
-        print(f"{str(datetime.datetime.utcnow())}: Persisted {df.shape[0]} rows.")
+        print(f"{str(datetime.datetime.utcnow())}: Persisted {row_df.shape[0]} rows.")
     except Exception as e:
-        print("{str(datetime.datetime.utcnow())}: Write failed")
+        print(f"{str(datetime.datetime.utcnow())}: Write failed")
         print(e)
 
+sdf = app.dataframe(input_topic)
+sdf = sdf.update(send_data_to_influx)
 
-def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
-    
-    # Buffer to batch rows every 250ms to reduce CPU overhead.
-    buffer = stream_consumer.timeseries.create_buffer()
-    buffer.time_span_in_milliseconds = 250
-    buffer.buffer_timeout = 250
-
-    buffer.on_dataframe_released = on_dataframe_received_handler
-
-
-# subscribe to new streams being received
-topic_consumer.on_stream_received = on_stream_received_handler
-
-print("Listening to streams. Press CTRL-C to exit.")
-
-# Handle termination signals and provide a graceful exit
-qx.App.run()
+if __name__ == "__main__":
+    print("Starting application")
+    app.run(sdf)
