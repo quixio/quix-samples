ca056c0ae4516d9ab3adb43d9d64e59860606f5d
merlin-quix <116729413+merlin-quix@users.noreply.github.com>
2024-02-20 17:39:23 +0700

    Update main.py

--- python/sources/InfluxDB-2.0/main.py
+++ python/sources/InfluxDB-2.0/main.py
@@ -1,15 +1,21 @@
-# Importing necessary libraries and modules
-from quixstreams import Application
-from quixstreams.models.serializers.quix import JSONSerializer, SerializationContext
+# Import basic utilities
 import os
 import random
-import influxdb_client
+import json
+import logging
 from time import sleep
-from datetime import datetime
-import pandas as pd
 
-# Create an Application
-app = Application.Quix(auto_create_topics=True)
+# import vendor-specfic modules
+from quixstreams import Application
+from quixstreams.models.serializers.quix import JSONSerializer, SerializationContext
+import influxdb_client
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Create a Quix Application
+app = Application.Quix(consumer_group="influxdbv2_sample", auto_create_topics=True)
 
 # Define a serializer for messages, using JSON Serializer for ease
 serializer = JSONSerializer()
@@ -18,83 +24,80 @@ serializer = JSONSerializer()
 topic_name = os.environ["output"]
 topic = app.topic(topic_name)
 
-client = influxdb_client.InfluxDBClient(token=os.environ["INFLUXDB_TOKEN"],
+influxdb2_client = influxdb_client.InfluxDBClient(token=os.environ["INFLUXDB_TOKEN"],
                         org=os.environ["INFLUXDB_ORG"],
                         url=os.environ['INFLUXDB_HOST'])
-query_api = client.query_api()
 
-measurement_name = os.environ.get("INFLUXDB_MEASUREMENT_NAME", os.environ["output"])
+query_api = influxdb2_client.query_api()
+
+measurement = os.environ.get("INFLUXDB_MEASUREMENT_NAME", os.environ["output"])
+interval = os.environ.get("task_interval", "5m")
+bucket = os.environ.get("INFLUXDB_BUCKET", "placeholder-bucket")
 
 # Global variable to control the main loop's execution
 run = True
 
 # Helper function to convert time intervals (like 1h, 2m) into seconds for easier processing.
 # This function is useful for determining the frequency of certain operations.
-def interval_to_seconds(interval):
-    if not interval:
-        raise ValueError("Invalid interval string")
+UNIT_SECONDS = {
+    "s": 1,
+    "m": 60,
+    "h": 3600,
+    "d": 86400,
+    "w": 604800,
+    "y": 31536000,
+}
 
-    unit = interval[-1].lower()
+def interval_to_seconds(interval: str) -> int:
     try:
-        value = int(interval[:-1])
-    except ValueError:
-        raise ValueError("Invalid interval format")
+        return int(interval[:-1]) * UNIT_SECONDS[interval[-1]]
+    except ValueError as e:
+        if "invalid literal" in str(e):
+            raise ValueError(
+                "interval format is {int}{unit} i.e. '10h'; "
+                f"valid units: {list(UNIT_SECONDS.keys())}")
+    except KeyError:
+        raise ValueError(
+            f"Unknown interval unit: {interval[-1]}; "
+            f"valid units: {list(UNIT_SECONDS.keys())}")
 
-    if unit == 's':
-        return value
-    elif unit == 'm':
-        return value * 60
-    elif unit == 'h':
-        return value * 3600
-    elif unit == 'd':
-        return value * 3600 * 24
-    elif unit == 'mo':
-        return value * 3600 * 24 * 30
-    elif unit == 'y':
-        return value * 3600 * 24 * 365
-    else:
-        raise ValueError(f"Unknown interval unit: {unit}")
-
-
-interval = os.environ.get("task_interval", "5m")
 interval_seconds = interval_to_seconds(interval)
-bucket = os.environ["INFLUXDB_BUCKET"]
-measurement = os.environ['INFLUXDB_MEASUREMENT_NAME']
 
 # Function to fetch data from InfluxDB and send it to Quix
 # It runs in a continuous loop, periodically fetching data based on the interval.
 def get_data():
-    # Run in a loop until the main thread is terminated |> range(start: -{interval})
-    while True:
-        start = datetime.now().timestamp()
-        flux_query = f'''
+    # Run in a loop until the main thread is terminated
+    while run:
+        try:            
+            # Query InfluxDB 2.0 using flux
+            flux_query = f'''
             from(bucket: "{bucket}")
                 |> range(start: -{interval})
                 |> filter(fn: (r) => r._measurement == "{measurement}")
                 |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
             '''
-        influx_df = query_api.query_data_frame(query=flux_query,org=os.environ['INFLUXDB_ORG'])
+            logger.info(f"Sending query: {flux_query}")
 
-        # Convert Timestamp columns to ISO 8601 formatted strings to avoid serialization errors
-        datetime_cols = influx_df.select_dtypes(include=['datetime64[ns]', 'datetime64[ns, UTC]']).columns
-        for col in datetime_cols:
-            influx_df[col] = influx_df[col].dt.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            table = query_api.query_data_frame(query=flux_query,org=os.environ['INFLUXDB_ORG'])
 
-        # If the query returns tables with different schemas, the result will be a list of dataframes.
-        if isinstance(influx_df, list):
-            for item in influx_df:
-                yield item
-                print(f"Published 1 row to Quix")
-        elif isinstance(influx_df, pd.DataFrame) and not influx_df.empty:
-            yield influx_df
-            print(f"Published {len(influx_df)} rows to Quix")
+            # Renaming time column to distinguish it from other timestamp types
+            table.rename(columns={'_time': 'original_time'}, inplace=True)
 
-        # Wait for the next interval
-        wait_time = datetime.now().timestamp() - start + interval_seconds
-        if wait_time > 0:
-            sleep(wait_time)
-        else:
-            print(f"Warning: Query took longer than {interval} seconds. Skipping sleep.")
+            # If there are rows to write to the stream at this time
+            if not table.empty:
+                json_result = table.to_json(orient='records', date_format='iso')
+                yield json_result
+                print("query success")
+            else:
+                print("No new data to publish.")
+
+            # Wait for the next interval
+            sleep(interval_seconds)
+
+        except Exception as e:
+            print("query failed", flush=True)
+            print(f"error: {e}", flush=True)
+            sleep(1)
 
 def main():
     """
@@ -108,19 +111,17 @@ def main():
     producer = app.get_producer()
 
     with producer:
-    # Iterate over the data from query result
-        for df in get_data():
-
-            # Generate a unique message_key for each row
-            for index, row in df.iterrows():
-                message_key = f"INFLUX_DATA_{str(random.randint(1, 100)).zfill(3)}_{index}"
-
-                # Convert the row to a dictionary
-                row_data = row.to_dict()
+        for res in get_data():
+            # Parse the JSON string into a Python object
+            records = json.loads(res)
+            for index, obj in enumerate(records):
+                # Generate a unique message_key for each row
+                message_key = f"INFLUX2_DATA_{str(random.randint(1, 100)).zfill(3)}_{index}"
+                logger.info(f"Produced message with key:{message_key}, value:{obj}")
 
                 # Serialize row value to bytes
                 serialized_value = serializer(
-                    value=row_data, ctx=SerializationContext(topic=topic.name)
+                    value=obj, ctx=SerializationContext(topic=topic.name)
                 )
 
                 # publish the data to the topic
