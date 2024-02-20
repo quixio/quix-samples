8650af53bf7f93c95354c587c86b6ea35f98da5e
Steve <100689438+SteveRosam@users.noreply.github.com>
2024-02-20 14:20:47 +0000

    remove print (#491)

--- python/transformations/starter-transformation/main.py
+++ python/transformations/starter-transformation/main.py
@@ -6,9 +6,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-print(os.environ["input"])
-
-
 with open("./.env", 'a+') as file: pass  # make sure the .env file exists
 load_dotenv("./.env")
 
