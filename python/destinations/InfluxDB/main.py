09de8d62a622a8ef798ed77ba554054eef9c8c72
merlin-quix <116729413+merlin-quix@users.noreply.github.com>
2024-02-28 23:01:16 +0700

    Improve influx samples (#496)
    * Merge start transformation bug fix to main
    
    * Fix paho-mqtt version to 1.6.1 to avoid picking breaking 2.0.0 (#493)
    
    * removing need to specify a measurement
    
    * adding hint about message key
    
    * improving message key hint
    
    * improving tag and field selection
    
    * making tag and field handling consistent
    
    * fixing weird default topic name
    
    ---------
    
    Co-authored-by: Steve <100689438+SteveRosam@users.noreply.github.com>
    Co-authored-by: PeterN <79838809+peter-quix@users.noreply.github.com>

--- python/destinations/InfluxDB/main.py
+++ python/destinations/InfluxDB/main.py
@@ -11,21 +11,19 @@ from influxdb_client_3 import InfluxDBClient3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+consumer_group_name = os.environ.get('CONSUMER_GROUP_NAME', "influxdb-data-writer")
 
-app = Application.Quix(consumer_group="influx-destination",
-                       auto_offset_reset="earliest")
+# Create a Quix platform-specific application instead
+app = Application.Quix(consumer_group=consumer_group_name, auto_create_topics=True, auto_offset_reset='earliest')
 
 input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
 
 # Read the environment variable and convert it to a dictionary
-tag_dict = ast.literal_eval(os.environ.get('INFLUXDB_TAG_COLUMNS', "{}"))
-
-# Read the environment variable for measurement name
-measurement_name = os.environ.get('INFLUXDB_MEASUREMENT_NAME', os.environ["input"])
+tag_keys = ast.literal_eval(os.environ.get('INFLUXDB_TAG_KEYS', "[]"))
+field_keys = ast.literal_eval(os.environ.get('INFLUXDB_FIELD_KEYS', "[]"))
 
 # Read the environment variable for the field(s) to get.
 # For multiple fields, use a list "['field1','field2']"
-field_keys = os.environ.get("INFLUXDB_FIELD_KEYS", "['field1','field2']")
                                            
 influx3_client = InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
                          host=os.environ["INFLUXDB_HOST"],
@@ -35,33 +33,43 @@ influx3_client = InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
 def send_data_to_influx(message):
     logger.info(f"Processing message: {message}")
     try:
-        quixtime = message['time']
-        # Get the name(s) and value(s) of the selected field(s)
-        # Using a single field in this example for simplicity
-        field1_name = field_keys[0]
-        field1_value = message[field_keys[0]]
+        # Get the measurement name to write data to
+        measurement_name = os.environ.get('INFLUXDB_MEASUREMENT_NAME', "measurement1")
 
-        logger.info(f"Using field keys: {', '.join(field_keys)}")
+        # Initialize the tags and fields dictionaries
+        tags = {}
+        fields = {}
 
-        # Using point dictionary structure
-        # See: https://docs.influxdata.com/influxdb/cloud-dedicated/reference/client-libraries/v3/python/#write-data-using-a-dict
+        # Iterate over the tag_dict and field_dict to populate tags and fields
+        for tag_key in tag_keys:
+            if tag_key in message:
+                tags[tag_key] = message[tag_key]
+
+        for field_key in field_keys:
+            if field_key in message:
+                fields[field_key] = message[field_key]
+
+        logger.info(f"Using tag keys: {', '.join(tags.keys())}")
+        logger.info(f"Using field keys: {', '.join(fields.keys())}")
+
+        # Construct the points dictionary
         points = {
             "measurement": measurement_name,
-            "tags": tag_dict,
-            "fields": {field1_name: field1_value},
-            "time": quixtime
+            "tags": tags,
+            "fields": fields,
+            "time": message['time']
         }
 
         influx3_client.write(record=points, write_precision="ms")
         
-        print(f"{str(datetime.datetime.utcnow())}: Persisted measurement to influx.")
+        logger.info(f"{str(datetime.datetime.utcnow())}: Persisted ponts to influx: {points}")
     except Exception as e:
-        print(f"{str(datetime.datetime.utcnow())}: Write failed")
-        print(e)
+        logger.info(f"{str(datetime.datetime.utcnow())}: Write failed")
+        logger.info(e)
 
 sdf = app.dataframe(input_topic)
 sdf = sdf.update(send_data_to_influx)
 
 if __name__ == "__main__":
-    print("Starting application")
+    logger.info("Starting application")
     app.run(sdf)
