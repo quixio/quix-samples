3f809ff9e731da218bcb94104c05e743699d8c17
tomas-quix <78492422+tomas-quix@users.noreply.github.com>
2024-03-01 09:46:00 +0100

    Event detection migrated to SDF (#498)

---
+++ python/transformations/Event-Detection/main.py
@@ -0,0 +1,39 @@
+import os
+from quixstreams import Application, State
+from datetime import datetime
+import json
+
+app = Application.Quix("hard-braking-v1", auto_offset_reset="earliest")
+
+input_topic = app.topic(os.environ["input"], value_deserializer='json')
+output_topic = app.topic(os.environ["output"], value_serializer='json')
+
+sdf = app.dataframe(input_topic)
+
+# Filter items out without brake value.
+sdf = sdf[sdf["Brake"].notnull()] 
+
+# Calculate hopping window of 1s with 200ms steps.
+sdf = sdf.apply(lambda row: row["Brake"]) \
+        .hopping_window(1000, 200).mean().final() 
+
+# Filter only windows where average brake force exceeded 50%.
+sdf = sdf[sdf["value"] > 0.5]
+
+# Create nice JSON alert message.
+sdf = sdf.apply(lambda row: {
+    "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
+    "Alert": {
+        "Title": "Hard braking detected.",
+        "Message": "For last 1 second, average braking power was " + str(row["value"])
+    }
+})
+
+# Print JSON messages in console.
+sdf = sdf.update(lambda row: print(json.dumps(row, indent=4)))
+
+# Send the message to the output topic
+sdf = sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
