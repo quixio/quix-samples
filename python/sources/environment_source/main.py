509961a2e2b8609d7ad43e56b2a15e735173ed74
Tomas Neubauer <tomas@quix.ai>
2024-09-11 20:37:31 +0200

    Environment source

---
+++ python/sources/environment_source/main.py
@@ -0,0 +1,34 @@
+import os
+from quixstreams import Application
+from quixstreams.sources.kafka import QuixEnvironmentSource
+
+# for local dev, load env vars from a .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Application()
+output_topic = app.topic(os.environ["output"])
+
+source_workspace_id = os.environ["source_workspace_id"]
+source_sdk_token = os.environ["source_sdk_token"]
+consumer_group = os.environ["consumer_group"]
+auto_offset_reset = os.environ["auto_offset_reset"]
+
+input_topic = QuixEnvironmentSource(
+    output_topic.name,
+    app.config,
+    output_topic.name,
+    quix_workspace_id=source_workspace_id, 
+    quix_sdk_token=source_sdk_token,
+    consumer_group=consumer_group,
+    auto_offset_reset=auto_offset_reset,
+    shutdown_timeout=30
+)
+
+sdf = app.dataframe(source=input_topic)
+
+#sdf.print()
+#sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
