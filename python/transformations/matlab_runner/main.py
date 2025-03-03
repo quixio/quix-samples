3de99224ca0c2f1282e0c9f77ce6866a1045ebec
steverosam <steve@quix.io>
2025-03-03 16:35:38 +0000

    Updates

--- python/transformations/matlab_runner/main.py
+++ python/transformations/matlab_runner/main.py
@@ -1,9 +1,11 @@
-print("Starting matlab")
-
 import matlab.engine
+
+print("Starting matlab")
 
 eng = matlab.engine.start_matlab()
 
+print("Calculate square root")
 output = eng.sqrt(8.0)
 
+print("Result:")
 print (output)
