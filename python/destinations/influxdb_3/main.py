a3d6679b2857d3b2ba7d76fd53eb2fa9dedbefc9
Tim Sawicki <tim@quix.io>
2025-04-22 16:32:20 -0400

    update in preparation for updated influxdb3sink

--- python/destinations/influxdb_3/main.py
+++ python/destinations/influxdb_3/main.py
@@ -13,43 +13,36 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# read the consumer group from config
-consumer_group_name = os.environ.get("CONSUMER_GROUP_NAME", "influxdb-data-writer")
-
-# read the timestamp column from config
-timestamp_column = os.environ.get("TIMESTAMP_COLUMN") if os.environ.get("TIMESTAMP_COLUMN") else None
-
-buffer_size = int(os.environ.get("BUFFER_SIZE", "1000"))
-
-buffer_delay = float(os.environ.get("BUFFER_DELAY", "1"))
-
 # Create a Quix platform-specific application instead
 app = Application(
-    consumer_group=consumer_group_name, 
+    consumer_group=os.environ.get("CONSUMER_GROUP_NAME", "influxdb-data-writer"),
     auto_offset_reset="earliest",
-    commit_every=buffer_size,
-    commit_interval=buffer_delay)
-    
-input_topic = app.topic(os.environ["input"])
+    commit_every=int(os.environ.get("BUFFER_SIZE", "1000")),
+    commit_interval=float(os.environ.get("BUFFER_DELAY", "1")),
+)
 
 # Read the environment variable and convert it to a dictionary
 tag_keys = os.environ.get("INFLUXDB_TAG_KEYS", "").split(",") if os.environ.get("INFLUXDB_TAG_KEYS") else []
 field_keys = os.environ.get("INFLUXDB_FIELD_KEYS", "").split(",")if os.environ.get("INFLUXDB_FIELD_KEYS") else []
 measurement_name = os.environ.get("INFLUXDB_MEASUREMENT_NAME", "measurement1")
+time_setter = os.environ.get("TIMESTAMP_COLUMN") if os.environ.get("TIMESTAMP_COLUMN") else None
+
 
 influxdb_v3_sink = InfluxDB3Sink(
-                            token=os.environ["INFLUXDB_TOKEN"],
-                            host=os.environ["INFLUXDB_HOST"],
-                            organization_id=os.environ["INFLUXDB_ORG"],
-                            tags_keys=tag_keys,
-                            fields_keys=field_keys,
-                            time_key=timestamp_column,
-                            database=os.environ["INFLUXDB_DATABASE"],
-                            measurement=measurement_name)
+    token=os.environ["INFLUXDB_TOKEN"],
+    host=os.environ["INFLUXDB_HOST"],
+    organization_id=os.environ["INFLUXDB_ORG"],
+    tags_keys=tag_keys,
+    fields_keys=field_keys,
+    time_setter=time_setter,
+    database=os.environ["INFLUXDB_DATABASE"],
+    measurement=measurement_name,
+)
+
+
+input_topic = app.topic(os.environ["input"])
 
 sdf = app.dataframe(input_topic)
-
-#sdf.print()
 sdf.sink(influxdb_v3_sink)
 
 if __name__ == "__main__":
