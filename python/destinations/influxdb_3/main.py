6c13b54f8eb82bd2fefd52746da262d9a4b79063
Tim Sawicki <tim@quix.io>
2025-04-23 01:15:57 -0400

    reorganize it and clean it up a bit

--- python/destinations/influxdb_3/main.py
+++ python/destinations/influxdb_3/main.py
@@ -1,6 +1,5 @@
 # import Utility modules
 import os
-import logging
 
 # import vendor-specific modules
 from quixstreams import Application
@@ -10,23 +9,11 @@ from quixstreams.sinks.core.influxdb3 import InfluxDB3Sink
 from dotenv import load_dotenv
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-# Create a Quix platform-specific application instead
-app = Application(
-    consumer_group=os.environ.get("CONSUMER_GROUP_NAME", "influxdb-data-writer"),
-    auto_offset_reset="earliest",
-    commit_every=int(os.environ.get("BUFFER_SIZE", "1000")),
-    commit_interval=float(os.environ.get("BUFFER_DELAY", "1")),
-)
-
-# Read the environment variable and convert it to a dictionary
 tag_keys = os.environ.get("INFLUXDB_TAG_KEYS", "").split(",") if os.environ.get("INFLUXDB_TAG_KEYS") else []
 field_keys = os.environ.get("INFLUXDB_FIELD_KEYS", "").split(",")if os.environ.get("INFLUXDB_FIELD_KEYS") else []
 measurement_name = os.environ.get("INFLUXDB_MEASUREMENT_NAME", "measurement1")
 time_setter = os.environ.get("TIMESTAMP_COLUMN") if os.environ.get("TIMESTAMP_COLUMN") else None
-
 
 influxdb_v3_sink = InfluxDB3Sink(
     token=os.environ["INFLUXDB_TOKEN"],
@@ -40,13 +27,17 @@ influxdb_v3_sink = InfluxDB3Sink(
 )
 
 
+app = Application(
+    consumer_group=os.environ.get("CONSUMER_GROUP_NAME", "influxdb-data-writer"),
+    auto_offset_reset="earliest",
+    commit_every=int(os.environ.get("BUFFER_SIZE", "1000")),
+    commit_interval=float(os.environ.get("BUFFER_DELAY", "1")),
+)
 input_topic = app.topic(os.environ["input"])
 
 sdf = app.dataframe(input_topic)
 sdf.sink(influxdb_v3_sink)
 
+
 if __name__ == "__main__":
-    logger.info("Starting application")
     app.run()
-
-
