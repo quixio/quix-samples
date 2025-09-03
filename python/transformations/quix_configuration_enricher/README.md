74b40028f11c70f938c2bfe85ce543abc70fcc2e
Tim Sawicki <tim@quix.io>
2025-09-03 16:01:00 -0400

    add quix config enricher

---
+++ python/transformations/quix_configuration_enricher/README.md
@@ -0,0 +1,44 @@
+# Quix Configuration Enricher
+
+[This code sample](https://github.com/quixio/quix-samples/tree/main/python/transformations/quix_configuration_enricher) demonstrates how to 
+enrich your data using a `Quix Configuration Service` (through a topic managed by the service).
+
+## How to run
+
+## How to run
+
+Create a [Quix](https://portal.platform.quix.io/signup?xlink=github) account or log-in and visit the Samples to use this project.
+
+Clicking `Edit code` on the Sample, forks the project to your own Git repo so you can customize it before deploying.
+
+## Environment Variables
+
+The connector uses the following environment variables:
+
+### Required
+- **DATA_TOPIC**: The topic containing your data to be enriched.
+- **CONFIG_TOPIC**: The topic managed by your Quix Configuration Service.
+- **OUTPUT_TOPIC**: The topic to write the enriched data to.
+- **LOOKUP_FIELDS_JSON**: a JSON-serialized string that contains the desired field names with corresponding config field references.  
+  ex: `{"f1": {"type": "cfg-name", "default": "value", "jsonpath": "path.to.f1"}, "f2": {"type": "cfg-name", "default": null, "jsonpath": "path.to.f2"}}`
+
+### Optional
+- **CONSUMER_GROUP_NAME**: The name of the consumer group to use when consuming from Kafka.  
+  Default: `postgres-sink`
+- **BATCH_SIZE**: The number of records that the sink holds before flushing data to PostgreSQL.  
+  Default: `1000`
+- **BATCH_TIMEOUT**: The number of seconds that the sink holds before flushing data to PostgreSQL.  
+  Default: `1`
+
+## Requirements / Prerequisites
+
+You will need to have a `Quix Configuration Service` instance running and any desired
+configurations actively available on its corresponding topic.
+
+## Contribute
+
+Submit forked projects to the Quix [GitHub](https://github.com/quixio/quix-samples) repo. Any new project that we accept will be attributed to you, and you'll receive $200 in Quix credit.
+
+## Open Source
+
+This project is open source under the Apache 2.0 license and available in our [GitHub](https://github.com/quixio/quix-samples) repo. Please star us and mention us on social media to show your appreciation.
---
+++ python/transformations/quix_configuration_enricher/main.py
@@ -0,0 +1,70 @@
+import os
+import json
+
+from quixstreams.dataframe.joins.lookups.quix_configuration_service import QuixConfigurationService
+from quixstreams.dataframe.joins.lookups.quix_configuration_service.lookup import JSONField
+from quixstreams import Application
+
+
+def get_fields():
+    """
+    The LOOKUP_FIELDS_JSON should be a JSON formatted like this example:
+    {
+        "f1": {"type": "cfg-name", "default": "value", "jsonpath": "path.to.f1"},
+        "f2": {"type": "cfg-name", "default": null, "jsonpath": "path.to.f2"},
+        "f3": {"type": "cfg-name", "default": 1.0, "jsonpath": "path.to.f3"},
+    }
+
+    If this environment-based approach is too restrictive, replace it with custom logic.
+    """
+    return {
+        field_name: JSONField(**field)
+        for field_name, field in json.loads(os.environ["LOOKUP_FIELDS_JSON"]).items()
+    }
+
+
+def main():
+    """
+    This template deploys a simple enricher using the QuixConfigurationService, which
+    enriches by performing a join by adding fields specified by the user in the
+    LOOKUP_FIELDS_JSON environment variable.
+
+    The join is achieved by retrieves configs from a specified topic maintained by a
+    `Quix Dynamic Configuration Service`.
+
+    The `Quix Dynamic Configuration Service` itself helps manage versioning of configs,
+    and the QuixConfigurationService helps streamline interacting with it.
+
+    The respective config applied is based on a combination of message key and the config "type"
+    specified.
+    """
+
+    app = Application(
+        consumer_group=os.environ["CONSUMER_GROUP_NAME"],
+        auto_offset_reset="earliest",
+        commit_interval=float(os.environ.get("BATCH_TIMEOUT", "1")),
+        commit_every=int(os.environ.get("BATCH_SIZE", "1000"))
+    )
+
+    data_topic = app.topic(name=os.environ["DATA_TOPIC"], key_deserializer="str")
+    config_topic = app.topic(name=os.environ["CONFIG_TOPIC"])
+    output_topic = app.topic(name=os.environ["OUTPUT_TOPIC"])
+    sdf = app.dataframe(topic=data_topic)
+
+    # Enrich data using the config service (lookup_join)
+    sdf = sdf.join_lookup(
+        lookup=QuixConfigurationService(
+            topic=config_topic,
+            app_config=app.config,
+        ),
+        fields=get_fields()
+    )
+
+    # Finish off by writing to the final result to the output topic
+    sdf.to_topic(output_topic)
+    # With our pipeline defined, now run the Application
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
