e76324c6aea52fd67d09dd3acdb8efd6b94b2e04
Tim Sawicki <tim@quix.io>
2025-10-14 12:21:05 -0400

    update tests and adjust a couple env var things

--- python/destinations/MQTT/main.py
+++ python/destinations/MQTT/main.py
@@ -1,13 +1,16 @@
-from mqtt import MQTTSink
 from quixstreams import Application
+from quixstreams.sinks.community.mqtt import MQTTSink
 import os
 
 # Load environment variables (useful when working locally)
 # from dotenv import load_dotenv
 # load_dotenv()
 
-app = Application(consumer_group="mqtt_consumer_group", auto_offset_reset="earliest")
-input_topic = app.topic(os.environ["input"], value_deserializer="double")
+app = Application(
+    consumer_group=os.getenv("CONSUMER_GROUP", "mqtt_consumer_group"),
+    auto_offset_reset="earliest"
+)
+input_topic = app.topic(os.environ["input"])
 
 sink = MQTTSink(
     client_id=os.environ["MQTT_CLIENT_ID"],
@@ -17,6 +20,7 @@ sink = MQTTSink(
     username=os.environ["MQTT_USERNAME"],
     password=os.environ["MQTT_PASSWORD"],
     version=os.environ["MQTT_VERSION"],
+    retain=os.getenv("MQTT_RETAIN_MESSAGES", "false").lower() == "true",
     tls_enabled=os.environ["MQTT_USE_TLS"].lower() == "true"
 )
 
