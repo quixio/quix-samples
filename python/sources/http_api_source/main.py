ae0f631adfc9e3da82708423c7e783746920a1f4
steverosam <steve@quix.io>
2026-03-10 14:06:07 +0000

    Secure with Basic or API key auth

--- python/sources/http_api_source/main.py
+++ python/sources/http_api_source/main.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import json
+import functools
 from flask import Flask, request, Response, redirect
 from flasgger import Swagger
 from waitress import serve
@@ -16,6 +17,33 @@ from dotenv import load_dotenv
 load_dotenv()
 
 service_url = os.environ["Quix__Deployment__Network__PublicUrl"]
+
+basic_auth_username = os.environ.get("basic_auth_username")
+basic_auth_password = os.environ.get("basic_auth_password")
+basic_auth_enabled = bool(basic_auth_username and basic_auth_password)
+
+api_key = os.environ.get("api_key")
+api_key_enabled = bool(api_key)
+
+auth_required = basic_auth_enabled or api_key_enabled
+
+
+def require_auth(f):
+    @functools.wraps(f)
+    def decorated(*args, **kwargs):
+        if auth_required:
+            if api_key_enabled and request.headers.get("X-API-Key") == api_key:
+                return f(*args, **kwargs)
+            if basic_auth_enabled:
+                auth = request.authorization
+                if auth and auth.username == basic_auth_username and auth.password == basic_auth_password:
+                    return f(*args, **kwargs)
+            return Response(
+                "Unauthorized", 401,
+                {"WWW-Authenticate": 'Basic realm="HTTP API Source"'}
+            )
+        return f(*args, **kwargs)
+    return decorated
 
 quix_app = Application()
 topic = quix_app.topic(os.environ["output"])
@@ -41,6 +69,7 @@ def redirect_to_swagger():
     return redirect("/apidocs/")
 
 @app.route("/data/", methods=['POST'])
+@require_auth
 def post_data_without_key():
     """
     Post data without key
@@ -66,6 +95,7 @@ def post_data_without_key():
     return Response(status=200)
 
 @app.route("/data/<key>", methods=['POST'])
+@require_auth
 def post_data_with_key(key: str):
     """
     Post data with a key
@@ -97,9 +127,15 @@ if __name__ == '__main__':
     print("=" * 60)
     print(" " * 20 + "CURL EXAMPLE")
     print("=" * 60)
+    if api_key_enabled:
+        auth_flag = f"\\\n    -H 'X-API-Key: {api_key}' "
+    elif basic_auth_enabled:
+        auth_flag = f"\\\n    -u '{basic_auth_username}:{basic_auth_password}' "
+    else:
+        auth_flag = ""
     print(
         f"""
-curl -L -X POST \\
+curl -L -X POST {auth_flag}\\
     -H 'Content-Type: application/json' \\
     -d '{{"key": "value"}}' \\
     {service_url}/data
